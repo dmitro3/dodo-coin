aeb417eac1712cc615a1efdc07b0fc9b6dbba273
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-16 22:23:41 +0330

    update get_refs/handler.ts

--- src/backend/api/player/get_refs/handler.ts
+++ src/backend/api/player/get_refs/handler.ts
@@ -1,9 +1,20 @@
 import Handler from "@backend/modules/Handler";
 import prisma from "@backend/modules/prisma/Prisma";
 import {userDetails} from "@backend/api/login/handler";
+import {getToken} from "@backend/utils/user";
 
 export default class UpgradeHandler extends Handler {
 	async handler() {
+		const user = await prisma.user.findUnique({
+			where: {
+				token: getToken(this.request)
+			},
+			include: {
+				refs: true
+			}
+		});
+
+		
 		return {
 			refs: [],
 			has_more: false
