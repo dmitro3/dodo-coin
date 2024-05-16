0bd5fc3d9e4220f7623e45a8fe92cf0ec03cedf1
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-16 22:34:57 +0330

    update send_invite/handler.ts

--- src/backend/api/player/send_invite/handler.ts
+++ src/backend/api/player/send_invite/handler.ts
@@ -3,6 +3,7 @@ import prisma from "@backend/modules/prisma/Prisma";
 import {userDetails} from "@backend/api/login/handler";
 import {getToken} from "@backend/utils/user";
 import {CLIENT_BOT} from "@/bot/main";
+import {Markup} from "telegraf";
 
 export default class UpgradeHandler extends Handler {
 	async handler() {
@@ -11,7 +12,11 @@ export default class UpgradeHandler extends Handler {
 		await CLIENT_BOT.waitToReady();
 		const link = `https://t.me/${CLIENT_BOT.me?.username}?start=${user.id}`
 		const text = `${link}\n🎁 +2.5k Shares as a first-time gift`;
-		await CLIENT_BOT.telegram.sendMessage(user.chatId,"" )
+		await CLIENT_BOT.telegram.sendMessage(user.chatId,"Invite your friends and get bonuses for each invited friend!", {
+			...Markup.inlineKeyboard([
+				Markup.button.switchToChat("Invite Friends!",text, true)
+			])
+		})
 		return {}
 	}
 }
