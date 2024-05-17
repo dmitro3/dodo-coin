4d97610ef9d8cc5eaabf7dded446a815546bc249
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-17 18:18:29 +0330

    update login/handler.ts

--- src/backend/api/login/handler.ts
+++ src/backend/api/login/handler.ts
@@ -356,7 +356,6 @@ export async function userDetails(user: Awaited<ReturnType<typeof prisma.user.fi
 
 export default class LoginHandler extends Handler {
     async handler() {
-        throw(500);
         const token = this.get('token', "Token Required");
         const user = await prisma.user.findUnique({
             where: {
