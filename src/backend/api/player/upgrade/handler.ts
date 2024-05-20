9e539b6b1bd7a9fbd9753d20fecebab944b8a48e
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-20 16:19:07 +0330

    update upgrade/handler.ts

--- src/backend/api/player/upgrade/handler.ts
+++ src/backend/api/player/upgrade/handler.ts
@@ -1,6 +1,7 @@
 import Handler from "@backend/modules/Handler";
-import prisma from "@backend/modules/prisma/Prisma";
-import {ChargeLevels, EnergyLevels, TapLevels, userDetails} from "@backend/api/login/handler";
+import prisma, {ChargeLevels, EnergyLevels, TapLevels} from "@backend/modules/prisma/Prisma";
+import {userDetails} from "@backend/api/login/handler";
+
 
 export default class UpgradeHandler extends Handler {
 	async handler() {
