6c808c82c8398f4658059d1234d62a111ac12088
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-20 15:29:06 +0330

    update src/instrumentation.ts

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,23 +1,34 @@
 import {ADMIN_BOT, HotReloadTelegramBot} from "./bot/main";
 import prisma from "@backend/modules/prisma/Prisma";
+import {User} from "@prisma/client";
 
-
+export let DEV_USER: User;
+export const DEV_LOGS: string[] = [];
 export async function register() {
 	HotReloadTelegramBot();
 
-	const origin = console.error;
 	const admin = await prisma.user.findFirst({
 		where: {
 			username: "itzunity"
 		}
 	});
-
-	console.log(`ADMIN LOG: @${admin?.username+""}`)
-
-	console.error = (...args: any[])=>{
-		origin(...args);
-		ADMIN_BOT.waitToReady().then(async (me)=>{
-			ADMIN_BOT.telegram.sendMessage(admin?.chatId+"", args.map(o => o+"").join("\n")).catch(origin);
-		}).catch(origin);
+	type keyType = 'error' | 'log' | 'warn';
+	const registerLog = (key: keyType) => {
+		const origin = console[key] as typeof console.error;
+		return (...args: any[])=>{
+			const R = origin(...args);
+			ADMIN_BOT.waitToReady().then(async (me)=>{
+				adminLog(args.map(o => o+"").join("\n"));
+			}).catch(origin);
+			return R;
+		};
 	}
+
+	for (const key of ['warn','log', 'error']) {
+		console[key as keyType] = registerLog(key as keyType);
+	}
+}
+
+export function adminLog(msg: string) {
+	DEV_LOGS.push(msg);
 }
