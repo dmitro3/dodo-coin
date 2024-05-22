ae0c32a2bcf485412d8c469ab7720f11e512f891
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 17:00:33 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import {notFound} from "next/navigation";
 import {Web3Modal} from "@/context/Web3Modal";
+import {useWeb3Modal} from "@web3modal/scaffold-react";
 
 const Page = () => {
+	const open = useWeb3Modal();
 	return (
 		<Web3Modal>
-			
+			<button onClick={()=>open.open()}>
+				open
+			</button>
 		</Web3Modal>
 	)
 };
