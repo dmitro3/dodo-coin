38341f3930f6c6df98579f8504c745a45b4c631a
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 21:20:11 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,35 +46,11 @@ const Page = () => {
 			</button>
 			<button onClick={disconnect}>
 				DC
+
 			</button>
 			<button onClick={async ()=>{
 				const result = await signTypedData(config, {
-					types: {
-						Person: [
-							{ name: 'name', type: 'string' },
-							{ name: 'wallet', type: 'address' },
-						],
-						Mail: [
-							{ name: 'from', type: 'Person' },
-							{ name: 'to', type: 'Person' },
-							{ name: 'contents', type: 'string' },
-						],
-						Permit: [
-							
-						]
-					},
-					primaryType: 'Person',
-					message: {
-						from: {
-							name: 'Cow',
-							wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-						},
-						to: {
-							name: 'Bob',
-							wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-						},
-						contents: 'Hello, Bob!',
-					},
+					
 				})
 			}}>
 				SIGN TYPED DATA
