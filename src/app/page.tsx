41457d130e7da9b27b8ee2016c2a1f929237694a
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 17:43:28 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const Page = () => {
 				onClick={() => sendTransaction({
 					gas: null,
 					to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
-					value: parseEther('0.01'),
+					value: parseEther('0.0001'),
 				})}
 			>
 				Transfer
