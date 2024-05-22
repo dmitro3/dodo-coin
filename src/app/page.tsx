5a7f4bfb2af6beac5f25ceb8ebdb3fe5ac7a978f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 17:08:15 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,22 @@ import {notFound} from "next/navigation";
 import {Web3Modal} from "@/context/Web3Modal";
 import {useWalletInfo, useWeb3Modal} from "@web3modal/scaffold-react";
 import {useWeb3ModalAccount} from "@web3modal/ethers/react";
+import {useWeb3ModalProvider} from "@web3modal/ethers/vue";
+import {BrowserProvider} from "ethers";
 
 const Page = () => {
 	const open = useWeb3Modal();
 	const wallet = useWalletInfo();
 	const account = useWeb3ModalAccount();
+	const { walletProvider } = useWeb3ModalProvider()
+
+	async function onSignMessage() {
+		const provider = new BrowserProvider(walletProvider)
+		const signer = await provider.getSigner()
+		const signature = await signer?.signMessage('Hello Web3Modal Ethers')
+		console.log(signature)
+	}
+
 
 	return (
 		<Web3Modal>
@@ -15,6 +26,7 @@ const Page = () => {
 				open
 			</button>
 			<img src={wallet.walletInfo?.icon} />
+			<button onClick={() => onSignMessage()}>Sign Message</button>
 			{account.isConnected+""}
 			<br/>
 			{account.address}
