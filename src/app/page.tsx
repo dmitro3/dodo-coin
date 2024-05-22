9864f5d6eb3f629d14c96cebc3b9e222e8b8bf2e
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 17:02:04 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import {useWalletInfo, useWeb3Modal} from "@web3modal/scaffold-react";
 const Page = () => {
 	const open = useWeb3Modal();
 	const wallet = useWalletInfo();
+	
 	return (
 		<Web3Modal>
 			<button onClick={()=>open.open()}>
