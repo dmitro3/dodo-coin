a5f24f8d6ce46bd90ed0e13bf7f8e172c33c677d
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 17:42:40 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ import {useWeb3ModalAccount} from "@web3modal/ethers/react";
 import {useWeb3ModalProvider} from "@web3modal/ethers/react";
 import {BrowserProvider} from "ethers";
 import Web3ModalProvider from "@/context/Web3Modal";
-import {useAccount, useSignMessage} from "wagmi";
+import {useAccount, useSendTransaction, useSignMessage} from "wagmi";
+import {parseEther} from "viem";
 
 const Page = () => {
 	const open = useWeb3Modal();
@@ -17,24 +18,33 @@ const Page = () => {
 
 	return (
 		<div>
-			<button onClick={()=>open.open()}>
+			<button onClick={() => open.open()}>
 				open
 			</button>
-			<img src={wallet.walletInfo?.icon} />
+			<img src={wallet.walletInfo?.icon}/>
 			<br/>
 			<button onClick={() => {
 				console.log(signMessage({
 					message: "test",
 				}))
-			}}>Sign Message</button>
+			}}>Sign Message
+			</button>
 			<br/>
-			{account.isConnected+""}
+			{account.isConnected + ""}
 			<br/>
 			{account.address}
 			<br/>
 			{account.chainId}
 			<br/>
-
+			<button
+				onClick={() => sendTransaction({
+					gas: null,
+					to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+					value: parseEther('0.01'),
+				})}
+			>
+				Transfer
+			</button>
 		</div>
 	)
 };
