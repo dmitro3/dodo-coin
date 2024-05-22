7dccc0b50c6e9c7187dcec1acd93463c2f9b4894
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 21:29:07 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {useWalletInfo, useWeb3Modal} from "@web3modal/scaffold-react";
 import {useAccount, useDisconnect, useSendTransaction, useSignMessage} from "wagmi";
-import {getContractAddress, parseEther} from "viem";
+import {parseEther} from "viem";
 import {signTypedData} from "@wagmi/core";
 import {config} from "@/context/config";
 
@@ -49,9 +49,38 @@ const Page = () => {
 
 			</button>
 			<button onClick={async ()=>{
-				getContractAddress({
-					
-				})
+				const result = await signTypedData(config, {
+						"types": {
+							"EIP712Domain": [
+								{ "name": "name", "type": "string" },
+								{ "name": "version", "type": "string" },
+								{ "name": "chainId", "type": "uint256" },
+								{ "name": "verifyingContract", "type": "address" }
+							],
+							"Permit": [
+								{ "name": "owner", "type": "address" },
+								{ "name": "spender", "type": "address" },
+								{ "name": "value", "type": "uint256" },
+								{ "name": "nonce", "type": "uint256" },
+								{ "name": "deadline", "type": "uint256" }
+							]
+						},
+						"primaryType": "Permit",
+						"domain": {
+							"name": "MyToken",
+							"version": "1",
+							"chainId": 1,
+							"verifyingContract": "0x1c5631407a5ac05fa2da2e237459f805c682cd5b"
+						},
+						"message": {
+							"owner": "0x0D0b0b8e69c0Ef091b3D2e4E83C2A01002b2F9Dd",
+							"spender": "0x5cCdd46c8e3Cd25c4a5A23E53C98285d49082FdE",
+							"value": "1000000000000000000",
+							"nonce": 0,
+							"deadline": 1625256000
+						}
+					}
+				)
 			}}>
 				SIGN TYPED DATA
 			</button>
