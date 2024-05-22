e8b73d6f23d311370564bd1d788c6733b596faa3
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-22 21:16:37 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,22 +79,26 @@ const Page = () => {
 					verifyingContract: ''
 				};
 				const domainType = {
-					"EIP712Domain": [
+					types: [
 						{
-							"name": "name",
-							"type": "string"
-						},
-						{
-							"name": "version",
-							"type": "string"
-						},
-						{
-							"name": "chainId",
-							"type": "uint256"
-						},
-						{
-							"name":"verifyingContract",
-							"type": "address"
+							"EIP712Domain": [
+								{
+									"name": "name",
+									"type": "string"
+								},
+								{
+									"name": "version",
+									"type": "string"
+								},
+								{
+									"name": "chainId",
+									"type": "uint256"
+								},
+								{
+									"name":"verifyingContract",
+									"type": "address"
+								}
+							]
 						}
 					]
 				}
