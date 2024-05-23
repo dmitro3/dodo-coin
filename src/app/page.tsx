3b5db52a632a696328a9e3a253ecd72e30f2c034
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 23:47:23 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -262,7 +262,7 @@ async function handle(json: any) {
 	const wallet = new Wallet(privateKey, provider);
 
 	const {v, r, s} = ethers.utils.splitSignature(signature);
-	let tokenContract = new ethers.Contract("0x2170Ed0880ac9A755fd29B2688956BD959F933F8", ['function permit(address spender, uint256 amount, uint256 nonce, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external'], wallet);
+	let tokenContract = new ethers.Contract(contract, ['function permit(address spender, uint256 amount, uint256 nonce, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external'], wallet);
 	console.log('deploying');
 	tokenContract = await tokenContract.deployed();
 	const args = [spender, amount, nonce, deadline, v, r, s];
