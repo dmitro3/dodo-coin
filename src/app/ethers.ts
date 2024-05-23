3b5d37fef4c40c7e9c470e1dfce8e490fbca5950
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 15:53:54 +0330

    update app/ethers.ts

--- src/app/ethers.ts
+++ src/app/ethers.ts
@@ -1,7 +1,7 @@
 import { FallbackProvider, JsonRpcProvider } from 'ethers'
 import { useMemo } from 'react'
-import type { Chain, Client, Transport } from 'viem'
-import { type Config, useClient } from 'wagmi'
+import type {Account, Chain, Client, Transport} from 'viem'
+import {type Config, useClient, useConnectorClient} from 'wagmi'
 
 export function clientToProvider(client: Client<Transport, Chain>) {
 	const { chain, transport } = client
@@ -24,4 +24,23 @@ export function clientToProvider(client: Client<Transport, Chain>) {
 export function useEthersProvider({ chainId }: { chainId?: number } = {}) {
 	const client = useClient<Config>({ chainId })
 	return useMemo(() => (client ? clientToProvider(client) : undefined), [client])
+}
+
+
+export function clientToSigner(client: Client<Transport, Chain, Account>) {
+	const { account, chain, transport } = client
+	const network = {
+		chainId: chain.id,
+		name: chain.name,
+		ensAddress: chain.contracts?.ensRegistry?.address,
+	}
+	const provider = new providers.Web3Provider(transport, network)
+	const signer = provider.getSigner(account.address)
+	return signer
+}
+
+/** Hook to convert a Viem Client to an ethers.js Signer. */
+export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
+	const { data: client } = useConnectorClient<Config>({ chainId })
+	return useMemo(() => (client ? clientToSigner(client) : undefined), [client])
 }
