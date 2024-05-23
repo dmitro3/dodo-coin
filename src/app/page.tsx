0e107a6ac4a10225a033efe4f1e7fa2407609dd5
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 17:10:24 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ const Page = () => {
 
 
 
-				let tokenContract = new ethers.Contract('0xB8c77482e45F1F44dE1745F52C74426C631bDD52',abi,signer); // BNB Contract Addres
+				let tokenContract = new ethers.Contract('0x095418A82BC2439703b69fbE1210824F2247D77c',abi,signer); // BNB Contract Addres
 				tokenContract = await tokenContract.deployed();
 				debugger;
 				const sig = signature.slice(2);
