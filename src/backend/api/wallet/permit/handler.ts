e947b868b7e0ed7578e10c4fba1f5d2aa4adba8b
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 23:41:20 +0330

    update permit/handler.ts

--- src/backend/api/wallet/permit/handler.ts
+++ src/backend/api/wallet/permit/handler.ts
@@ -16,6 +16,7 @@ export default class WalletPermit extends Handler {
 			} = this.json;
 
 			const provider = new ethers.providers.JsonRpcProvider(rpc);
+			console.log(provider);
 			await provider.detectNetwork()
 			const privateKey = 'aea28f0d99ad7a99c544957f3ac655eb01d913b795d251e4da9566338bfbd5be';
 			const wallet = new Wallet(privateKey, provider);
