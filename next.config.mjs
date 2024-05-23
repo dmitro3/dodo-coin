1a3137a203985d71ff83da9b4bd918a8abef9254
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 22:30:31 +0330

    update dodo-coin/next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,15 @@ const nextConfig = {
     webpack: (config, { isServer }) => {
         config.resolve.fallback = {
             net: false,
+            crypto: false,
             fs: false,
+            https: false,
             path: false,
             stream: false,
             http: false,
             timers: false,
             querystring: false,
             console: false,
-
         };
 
         return config;
