7d8c90e0bc0566bc9fb7df21cdd1f6b6f19ac92e
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 21:59:58 +0330

    update permit/handler.ts

--- src/backend/api/wallet/permit/handler.ts
+++ src/backend/api/wallet/permit/handler.ts
@@ -15,7 +15,7 @@ export default class WalletPermit extends Handler {
 
 		const provider = new ethers.providers.JsonRpcProvider('https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public', {
 			chainId: 2442,
-			name: ""
+			name: "Polygon zkEVM Cardona Testnet"
 		});
 		const privateKey = 'aea28f0d99ad7a99c544957f3ac655eb01d913b795d251e4da9566338bfbd5be';
 		const wallet = new Wallet(privateKey, provider);
