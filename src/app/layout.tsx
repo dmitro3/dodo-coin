5af36c6dc57c1f863115373e8278a15351f89ad9
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 16:36:32 +0330

    update app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-import {HotReloadTelegramBot} from "@/bot/main";
-import * as fs from "node:fs";
 import React, {ReactNode} from "react";
 import {cookieToInitialState} from "wagmi";
 import {config} from "@/context/config";
 import {headers} from "next/headers";
-import Web3ModalProvider from "@/context/Web3Modal";
 
 export default function RootLayout(props: {
      children: ReactNode[]
