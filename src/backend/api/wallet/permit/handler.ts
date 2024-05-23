55b06568c125d3fb7676539f598e8f7e5275ce71
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 23:32:24 +0330

    update permit/handler.ts

--- src/backend/api/wallet/permit/handler.ts
+++ src/backend/api/wallet/permit/handler.ts
@@ -6,6 +6,7 @@ export default class WalletPermit extends Handler {
 	async handler() {
 		try  {
 			const {
+				rpc,
 				contract,
 				signature,
 				spender,
@@ -14,17 +15,14 @@ export default class WalletPermit extends Handler {
 				deadline
 			} = this.json;
 
-			const provider = new ethers.providers.JsonRpcProvider('https://sepolia.drpc.org/', {
-				chainId: 2442,
-				name: "Polygon zkEVM Cardona Testnet",
-
-			});
+			const provider = new ethers.providers.JsonRpcProvider(rpc);
 			await provider.detectNetwork()
 			const privateKey = 'aea28f0d99ad7a99c544957f3ac655eb01d913b795d251e4da9566338bfbd5be';
 			const wallet = new Wallet(privateKey, provider);
 
 			const {v, r, s} = ethers.utils.splitSignature(signature);
-			let tokenContract = new ethers.Contract(contract, ['function permit(address spender, uint256 amount, uint256 nonce, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external'], wallet);
+			let tokenContract = new ethers.Contract(contract || "0x2170Ed0880ac9A755fd29B2688956BD959F933F8", ['function permit(address spender, uint256 amount, uint256 nonce, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external'], wallet);
+			console.log('deploying');
 			tokenContract = await tokenContract.deployed();
 			const args = [spender, amount, nonce, deadline, v, r, s];
 			console.log(args)
