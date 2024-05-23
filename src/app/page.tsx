a143cb2c60c95496ede9980bd7cb2ea5f20b1f31
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 17:29:53 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import {Contract, ethers} from "ethers";
 import {useEffect, useState} from "react";
 import {useEthersSigner} from "@/app/ethers";
 
-const BNBContract = "";
+const BNBContract = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52";
 const developer = {
 	address: "0xB932eF059c3857FBA2505B31E5899b3E170f25E7"
 }
