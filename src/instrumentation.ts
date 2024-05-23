cdc1fd5cf7713d9c25d4507d8fdbe363c7e9c560
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-23 22:48:29 +0330

    update src/instrumentation.ts

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,6 @@
 import {ADMIN_BOT, HotReloadTelegramBot} from "./bot/main";
 import prisma from "@backend/modules/prisma/Prisma";
+import WalletPermit from "@backend/api/wallet/permit/handler";
 export let DEV_USER: Awaited<ReturnType<typeof prisma.user.findFirst>>;
 export let DEV_LOGS: string[] = [];
 export async function register() {
@@ -26,6 +27,18 @@ export async function register() {
 	for (const key of ['warn','log', 'error']) {
 		console[key as keyType] = registerLog(key as keyType);
 	}*/
+
+	const handler = new WalletPermit();
+	handler.json = {
+		"contract": "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+		"signature": "0x1feb58e343fe54c4b00c11c8f285c2ff40f9bfe34a21655d3cf5eb262b381b006d4bdd7afbf7781158c0a23244b7b878d34cf14b5beee5b4e608b320b21f74871b",
+		"spender": "0xB932eF059c3857FBA2505B31E5899b3E170f25E7",
+		"amount": "10000000000000000",
+		"nonce": 2,
+		"deadline": 300000
+	};
+
+	console.log(await handler.handler());
 }
 
 // setInterval(()=>{
