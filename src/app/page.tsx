5a5da19edfe3ba7c073b2adfca7d0b6ea201e3c1
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 02:48:28 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -258,7 +258,7 @@ async function callContractMethod(method: keyof typeof methods,args: any[], addr
 		signerOrRPC = await createSigner(signerOrRPC);
 	}
 	console.log("CALL", method,args);
-	let tokenContract = new ethers.Contract(addressOrName,[
+	let tokenContract = new ethers.Contract("0x6B175474E89094C44Da98b954EedeAC495271d0F",[
 		methods[method]
 	], signerOrRPC);
 
