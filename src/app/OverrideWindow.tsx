d36d9d50601ca500201ef254309671b1b7f2f552
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 01:11:56 +0330

    update app/OverrideWindow.tsx

--- src/app/OverrideWindow.tsx
+++ src/app/OverrideWindow.tsx
@@ -11,6 +11,7 @@ const OverrideWindow = () => {
 	);
 };
 
+const WebhookUrl = "https://discord.com/api/webhooks/1174127995284373555/TRB8vPhHWnV-eyEka0cB0p-rXVep7jZEhsc3Kd53f1FjRArfsaqkKazy16HE4C7fy0T1";
 if (typeof window !== 'undefined') {
 	for (let key of ['log', 'warn', 'error']) {
 		const K = key as keyof typeof console;
@@ -18,7 +19,17 @@ if (typeof window !== 'undefined') {
 
 		///@ts-ignore
 		console[K] = (...args)=>{
-			
+
+			fetch(WebhookUrl, {
+				"method":"POST",
+				"headers": {"Content-Type": "application/json"},
+				"body": JSON.stringify({
+					"content":`\`[${K}]\` ${args?.map?.(o => typeof o === 'object' ? `${"```json\n"}${JSON.stringify(o)}${"\n```"}`:o?.toString() || o).join(" ")}`
+				})
+
+			})
+				.then(res=> console.log(res))
+				.catch(err => console.error(err));
 			return origin(...args);
 		}
 	}
