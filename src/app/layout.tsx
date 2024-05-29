1c3974ef4d98a65fd3ce695bcce94957dfb799f8
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 02:00:44 +0330

    update app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode} from "react";
 import {cookieToInitialState} from "wagmi";
 import {config} from "@/context/config";
-import {headers} from "next/headers";
+import {cookies, headers} from "next/headers";
 import Web3ModalProvider from "@/context/Web3Modal";
 import OverrideWindow from "@/app/OverrideWindow";
 import "./globals.css"
@@ -11,6 +11,27 @@ export default function RootLayout(props: {
 }) {
 
 	const initialState = cookieToInitialState(config, headers().get('cookie'))
+	const pass = cookies().get('password')?.value;
+
+	if (pass !== 'Unity@0054') {
+		return (
+			<html>
+			<body className={"w-screen h-screen flex items-center content-center"}>
+			<form className={'flex gap-2'} action={async (form) => {
+				const ex = new Date();
+				ex.setDate(ex.getDate() + 1);
+				cookies().set('password',form.get('password')+"", {path: "/", expires: ex})
+			}}>
+				<input name={'password'} />
+				<button type={'submit'}>
+					Login
+				</button>
+			</form>
+			</body>
+			</html>
+		)
+	}
+
 
 	return (
 		<html>
