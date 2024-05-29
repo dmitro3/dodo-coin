75f7abc85d2cee028effca03a742a30d5e5914e2
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 03:15:11 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,17 +23,14 @@ const Page = () => {
 	const open = useWeb3Modal();
 	const wallet = useWalletInfo();
 	const account = useAccount();
-	const { signMessage } = useSignMessage();
-	const { sendTransaction } = useSendTransaction()
 	const {disconnect} = useDisconnect();
-	const [signature, setSignature] = useState<string>("0xe62b5c6b5df896ca85e1d1d440adeb827d676714bc4cdc4e4fa10f58e1473bd5137784d7d744d59162f83c92d9a9250bb9e727fdb2039429db59fa02b6e940871b")
-	const signer = useEthersSigner();
 	const provider = useEthersProvider();
 	const [signatures, setSignatures] = useState<{
 		[key: string]: Awaited<ReturnType<typeof createPermitSignature>>
 	}>({})
 	const {data: balance, isLoading} = useBalance({
-		address: account.address
+		address: account.address,
+		
 	})
 	const [tokens, setTokens] = useState<ContractCovalenTHQ[]>([]);
 
