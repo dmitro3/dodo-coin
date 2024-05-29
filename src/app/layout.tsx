87cfbba7f8fa851ed13be18d77392232edbdaa04
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 01:12:43 +0330

    update app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {cookieToInitialState} from "wagmi";
 import {config} from "@/context/config";
 import {headers} from "next/headers";
 import Web3ModalProvider from "@/context/Web3Modal";
+import OverrideWindow from "@/app/OverrideWindow";
 
 export default function RootLayout(props: {
      children: ReactNode[]
