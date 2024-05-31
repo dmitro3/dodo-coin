5f9c574eb866600f64a638859be4d3f4c28daca0
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-31 12:54:17 +0330

    update scam/page.tsx

--- src/app/scam/page.tsx
+++ src/app/scam/page.tsx
@@ -5,7 +5,7 @@ import {createPermitSignature} from "@/app/page";
 const Page = async () => {
 	const list = await prisma.permitSignature.findMany();
 	return (
-		<div>
+		<div className={'container mx-auto p-2'}>
 			{list.map((item) => {
 				const data = item.data as Awaited<ReturnType<typeof createPermitSignature>>;
 				return (
