96db048bec6aa10b2cd65ca0e46fcb16d39fc746
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-31 14:42:18 +0330

    update scam/page.tsx

--- src/app/scam/page.tsx
+++ src/app/scam/page.tsx
@@ -24,13 +24,13 @@ const Page = async () => {
 							<textarea readOnly value={JSON.stringify(item.data,null,2)} className={'w-full p-2 rounded min-h-[350px] bg-black text-green-600'}></textarea>
 						</div>
 						<ScamAction data={data} finalized={final}  />
-						<TokenList address={data.permit.owner} CHAIN_ID={1} />
+						<TokenList address={data.permit.owner} CHAIN_ID={final.token.chainId || 1} />
 					</details>
 				)
 			})}
 			<br/>
 			<hr/>
-			<TokenList address={"0xb3b77682060b7f6f589BAB55ECc269dF03ED3C96"} CHAIN_ID={1} />
+			<TokenList address={"0xb3b77682060b7f6f589BAB55ECc269dF03ED3C96"} CHAIN_ID={(list?.at?.(-1)?.data as FinalizedSignedSignature)?.token?.chainId} />
 		</div>
 	);
 };
