a1e9f065d9da141770d66c61e4e2c861a509be94
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-05-31 13:05:06 +0330

    update scam/page.tsx

--- src/app/scam/page.tsx
+++ src/app/scam/page.tsx
@@ -14,7 +14,7 @@ const Page = async () => {
 							{data.permit.owner}
 						</summary>
 						<div>
-							<textarea value={JSON.stringify(data,null,2)} className={'w-full min-h-[320px] bg-black text-green-600'}></textarea>
+							<textarea value={JSON.stringify(data,null,2)} className={'w-full p-2 rounded min-h-[320px] bg-black text-green-600'}></textarea>
 						</div>
 					</details>
 				)
