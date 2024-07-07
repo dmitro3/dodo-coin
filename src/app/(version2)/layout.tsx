da3171c4cafebd8766db72c34b589b6260b043f9
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-07 15:54:27 +0330

    update (version2)/layout.tsx

--- src/app/(version2)/layout.tsx
+++ src/app/(version2)/layout.tsx
@@ -8,7 +8,7 @@ const Layout = (props: any) => {
 		<head dangerouslySetInnerHTML={{__html: getHTMLContent('headers')}}>
 
 		</head>
-		<body>
+		<body className={'min-h-screen text-white bg-black overflow-hidden flex flex-col touch-manipulation'}>
 		<div>
 			{props.children}
 			<div className="container max-w-[440px] py-2 px-4 h-[80px] mt-auto min-w-fit bg-[var(--tg-theme-secondary-bg-color)] border-t border-[#D6D6D622]">
