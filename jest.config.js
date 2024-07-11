5e1620062193056e17655b6b5d785a1a7a85c91e
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-11 23:03:22 +0330

    update dodo-coin/jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
-    testRegex: '/test/.*.(ts|tsx)$',
+    testRegex: '/test/*.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     "moduleNameMapper": {
         "@/(.*)": "<rootDir>/src/$1",
