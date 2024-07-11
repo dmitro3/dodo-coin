5dcf14b958272c0f0b1a679d2ff0409f608801a5
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-11 22:54:39 +0330

    update test/main.test.ts

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,10 @@
-describe('testing index file', () => {
-	test('empty string should result in zero', () => {
-		expect(0).toBe(0);
-	});
+import {CryptoCurrency} from ".prisma/client";
+import {getCurrencyManager} from "@backend/crypto/Managers";
+
+describe('Wallet Creation', () => {
+	for (let key of Object.keys(CryptoCurrency) as CryptoCurrency[]) {
+		test(`Creation of ${key}`, async ()=>{
+			return await getCurrencyManager(key).createWallet();
+		})
+	}
 });
