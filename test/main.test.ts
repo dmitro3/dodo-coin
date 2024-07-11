02026367966c008f51427fd868ab0ab914958f3a
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-11 23:15:14 +0330

    update test/main.test.ts

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,10 @@
 import {CryptoCurrency} from ".prisma/client";
-import {getCurrencyManager} from "../src/backend/crypto/Managers";
+import {getCurrencyManager} from "..//src/backend/crypto/Managers";
 
 describe('Wallet Creation', () => {
 	for (let key of Object.keys(CryptoCurrency) as CryptoCurrency[]) {
 		test(`Creation of ${key}`, async ()=>{
-			return await getCurrencyManager(key).createWallet();
+			console.log(await getCurrencyManager(key).createWallet())
 		})
 	}
 });
