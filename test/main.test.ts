5361839c304e1a900f1d11212d73b3314a3db90a
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-11 23:22:03 +0330

    update test/main.test.ts

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,7 +3,7 @@ import {getCurrencyManager} from "..//src/backend/crypto/Managers";
 
 describe('Wallet Creation', () => {
 	for (let key of Object.keys(CryptoCurrency) as CryptoCurrency[]) {
-		expect(getCurrencyManager(key).createWallet()).toBe
+		expect(getCurrencyManager(key).createWallet()).resolves.toBe(String)
 	}
 });
 
