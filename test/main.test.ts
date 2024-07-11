235112a0d793cf67fb4ba211c0b0b91535c92c1f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-11 23:25:44 +0330

    update test/main.test.ts

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,16 +3,9 @@ import {getCurrencyManager} from "..//src/backend/crypto/Managers";
 
 describe('Wallet Creation', () => {
 	for (let key of Object.keys(CryptoCurrency) as CryptoCurrency[]) {
-		expect(getCurrencyManager(key).createWallet()).resolves.toContain({
-			privateKey: isString,
-			address: String,
-			publicKey: String || undefined
+		test(`Creation of ${key}`, async ()=>{
+
+			return (await getCurrencyManager(key).createWallet()).address
 		})
 	}
 });
-
-describe('Wallet HAlo', () => {
-	test("Test", async ()=>{
-		return "HALO";
-	})
-})
