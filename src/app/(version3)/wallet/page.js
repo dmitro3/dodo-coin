dbe1a5c1558fff0e98713afbebf9e413278c99ae
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 23:39:00 +0330

    update wallet/page.js

--- src/app/(version3)/wallet/page.js
+++ src/app/(version3)/wallet/page.js
@@ -140,24 +140,16 @@ function PageV2() {
                             </filter>
                         </defs>
                     </svg>
-                    <svg width="275" height="275" viewBox="0 0 275 275" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_f_1464_6497)">
-                            <circle cx="137.529" cy="137.471" r="72.4143" fill="#f5ee00"></circle>
-                        </g>
-                        <circle cx="137" cy="138" r="63.4286" fill="white" fill-opacity="0.05"></circle>
-                        <circle cx="137" cy="138" r="74" fill="white" fill-opacity="0.05"></circle>
-                        <defs>
-                            <filter id="filter0_f_1464_6497" x="0.0999756" y="0.0428467" width="274.857"
-                                    height="274.857"
-                                    filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
-                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix"
-                                         result="shape"></feBlend>
-                                <feGaussianBlur stdDeviation="32.5071"
-                                                result="effect1_foregroundBlur_1464_6497"></feGaussianBlur>
-                            </filter>
-                        </defs>
+
+                    <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M28 56C43.464 56 56 43.464 56 28C56 12.536 43.464 0 28 0C12.536 0 0 12.536 0 28C0 43.464 12.536 56 28 56Z"
+                            fill="#0098EA"/>
+                        <path
+                            d="M37.5603 15.6277H18.4386C14.9228 15.6277 12.6944 19.4202 14.4632 22.4861L26.2644 42.9409C27.0345 44.2765 28.9644 44.2765 29.7345 42.9409L41.5381 22.4861C43.3045 19.4251 41.0761 15.6277 37.5627 15.6277H37.5603ZM26.2548 36.8068L23.6847 31.8327L17.4833 20.7414C17.0742 20.0315 17.5795 19.1218 18.4362 19.1218H26.2524V36.8092L26.2548 36.8068ZM38.5108 20.739L32.3118 31.8351L29.7417 36.8068V19.1194H37.5579C38.4146 19.1194 38.9199 20.0291 38.5108 20.739Z"
+                            fill="white"/>
                     </svg>
+
                 </div>
             </main>
             <Navbar/>
