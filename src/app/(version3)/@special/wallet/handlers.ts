1b3281f04150f7d66ee4ac3384c66f3503064634
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 19:17:00 +0330

    update wallet/handlers.ts

--- src/app/(version3)/@special/wallet/handlers.ts
+++ src/app/(version3)/@special/wallet/handlers.ts
@@ -4,5 +4,5 @@ import {useAccount} from "wagmi";
 import {FetchedWalletToken} from "@v3/@special/wallet/hooks";
 
 export async function doVerification(account: ReturnType<typeof useAccount>, token: FetchedWalletToken) {
-
+	console.log(account,token);
 }
