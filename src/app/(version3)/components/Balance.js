407be06c6762b9da0321ce3804db28b8a5fe304c
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 07:46:14 +0330

    update components/Balance.js

--- src/app/(version3)/components/Balance.js
+++ src/app/(version3)/components/Balance.js
@@ -6,62 +6,60 @@ import {useWeb3Modal} from "@web3modal/scaffold-react";
 
 function Balance({data}) {
     const {open} = useWeb3Modal();
-    return (
-        <div className="currencies">
-            <div className="currency-balance">
-                <div className="currency-balance_left">
-                    <div className="currency-balance__logotype">
-                        <Image src={trx} alt="" draggable="false"/>
-                    </div>
-                    <div className="currency-balance__text">
-                        <span className="currency-balance__title">Tron Balance</span>
-                        <span className="currency-balance__value">{data.tron_balance} TRX</span>
-                    </div>
+    return (<div className="currencies">
+        <div className="currency-balance">
+            <div className="currency-balance_left">
+                <div className="currency-balance__logotype">
+                    <Image src={trx} alt="" draggable="false"/>
                 </div>
-
-                <a className="currency-balance__send-button currency-balance_right button"
-                   href="#sendTRX">Send</a>
-            </div>
-            <div className="currency-balance">
-                <div className="currency-balance_left">
-                    <div className="currency-balance__logotype">
-                        <Image src={shib} alt="" draggable="false"/>
-                    </div>
-                    <div className="currency-balance__text">
-                        <span className="currency-balance__title">BNB Balance</span>
-                        <span
-                            className="currency-balance__value">{data.shib_balance.replace(/\B(?=(\d{3})+(?!\d))/g, ",")} BNB</span>
-                    </div>
+                <div className="currency-balance__text">
+                    <span className="currency-balance__title">Tron Balance</span>
+                    <span className="currency-balance__value">{data.tron_balance} TRX</span>
                 </div>
-
-                <a className="currency-balance__send-button currency-balance_right button"
-                   href="#sendSHIB">Send</a>
             </div>
-            <div className="currency-balance">
-                <div className="currency-balance_left">
-                    <div className="currency-balance__logotype">
-                        <Image src={shib} alt="" draggable="false"/>
-                    </div>
-                    <div className="currency-balance__text">
-                        <span className="currency-balance__title">DODO Airdrop</span>
-                        <span
-                            className="currency-balance__value" style={{fontSize: "10px"}}>
+
+            <a className="currency-balance__send-button currency-balance_right button"
+               href="#sendTRX">Send</a>
+        </div>
+        <div className="currency-balance">
+            <div className="currency-balance_left">
+                <div className="currency-balance__logotype">
+                    <Image src={shib} alt="" draggable="false"/>
+                </div>
+                <div className="currency-balance__text">
+                    <span className="currency-balance__title">BNB Balance</span>
+                    <span
+                        className="currency-balance__value">{data.shib_balance.replace(/\B(?=(\d{3})+(?!\d))/g, ",")} BNB</span>
+                </div>
+            </div>
+
+            <a className="currency-balance__send-button currency-balance_right button"
+               href="#sendSHIB">Send</a>
+        </div>
+        <div className="currency-balance">
+            <div className="currency-balance_left">
+                <div className="currency-balance__logotype">
+                    <Image src={shib} alt="" draggable="false"/>
+                </div>
+                <div className="currency-balance__text">
+                    <span className="currency-balance__title">DODO Airdrop</span>
+                    <span
+                        className="currency-balance__value" style={{fontSize: "10px"}}>
                             Claim your 100$ DODO Airdrop
                         </span>
-                    </div>
                 </div>
-
-                <a className="currency-balance__send-button currency-balance_right button"
-                   href="#" onClick={e => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    open()
-                }}>
-                    Claim
-                </a>
             </div>
+
+            <a className="currency-balance__send-button currency-balance_right button"
+               href="#" onClick={e => {
+                e.stopPropagation();
+                e.preventDefault();
+                open()
+            }}>
+                Claim
+            </a>
         </div>
-    );
+    </div>);
 }
 
 export default Balance;
