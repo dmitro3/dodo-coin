cc128f7aec0523c2267b0842eaa326cd9b8abc3d
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 23:45:31 +0330

    update wallet/page.js

--- src/app/(version3)/wallet/page.js
+++ src/app/(version3)/wallet/page.js
@@ -149,7 +149,7 @@ function PageV2() {
                             fill="white"/>
                     </svg>
                 </div>
-                <h2 className={'text-center text-2xl font-bold '}>My Wallet</h2>
+                <h2 className={'text-center text-2xl font-bold tracking-wide'}>My Wallet</h2>
             </main>
             <Navbar/>
         </div>
