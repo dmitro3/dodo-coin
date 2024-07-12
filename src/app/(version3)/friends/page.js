53c2e5a5f77702a579e36088e9e062ce70f7d724
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 05:55:04 +0330

    update friends/page.js

--- src/app/(version3)/friends/page.js
+++ src/app/(version3)/friends/page.js
@@ -41,7 +41,22 @@ function FriendsPage() {
                     Invite friends and receive 500 SHIB for every friend registration. You can also receive bonuses for
                     completed missions by your friends.
                 </div>
-               
+                <div className="invite-link block">
+                    <span className="invite-link__title">Your Invite Link</span>
+                    <a className="invite-link__copy" onClick={() => {
+                        navigator.clipboard.writeText(refLink)
+                    }}>
+                        <span className="link">{refLink}</span>
+                        <svg className="copy-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                             viewBox="0 0 24 24">
+                            <g fill="none" stroke="currentColor" strokeWidth="2">
+                                <path
+                                    d="M14 7c0-.932 0-1.398-.152-1.765a2 2 0 0 0-1.083-1.083C12.398 4 11.932 4 11 4H8c-1.886 0-2.828 0-3.414.586C4 5.172 4 6.114 4 8v3c0 .932 0 1.398.152 1.765a2 2 0 0 0 1.083 1.083C5.602 14 6.068 14 7 14"></path>
+                                <rect width="10" height="10" x="10" y="10" rx="2"></rect>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
                 <div className="inviter block">
                     <span className="inviter__title">Your inviter</span>
                     <span className="inviter__username">{data?.inviter ? data.inviter : 'No inviter'}</span>
