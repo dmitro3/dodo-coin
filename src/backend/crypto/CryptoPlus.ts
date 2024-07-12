956f92ce623486edee6aab28e5b11e7dbe59b057
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 06:38:12 +0330

    update crypto/CryptoPlus.ts

--- src/backend/crypto/CryptoPlus.ts
+++ src/backend/crypto/CryptoPlus.ts
@@ -1,5 +1,11 @@
 'use server';
 
 export async function createPosPayment(amount: number) {
-	return await fetch("")
+	return await fetch("https://api.cryptocloud.plus/v2/invoice/pos/create?locale=en", {
+		headers: {
+			"content-type": "application/json",
+		},
+		method: "POST",
+		
+	})
 }
