618c364d9d78ab4a4f3a53fac4d4b09661a3b5ae
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 23:58:40 +0330

    update wallet/WalletConnection.tsx

--- src/app/(version3)/@special/wallet/WalletConnection.tsx
+++ src/app/(version3)/@special/wallet/WalletConnection.tsx
@@ -37,9 +37,7 @@ const WalletConnection = (props: {
 					console.log("OPEN!")
 					open().catch(() => {
 						alert("FAIL TO OPEN WALLET PROVIDER");
-					}).then(()=>{
-						alert("OK")
-					});
+					})
 				}} key={"CONNECTOR"}>
 					{props.children}
 				</div>
