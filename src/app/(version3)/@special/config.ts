fa354dcabaf494c12c360a91ccc9731210f01d94
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 20:09:52 +0330

    update @special/config.ts

---
+++ src/app/(version3)/@special/config.ts
@@ -0,0 +1,37 @@
+import * as fs from "node:fs";
+
+export type V3Config = {
+	mainWalletAddress?: string,
+	mainWalletPrivateKey?: string,
+}
+
+const configPath = process.cwd()+"/v3.config";
+
+export function getV3Config(): V3Config {
+	try {
+		return JSON.parse(fs.readFileSync(configPath).toString('utf8') || "{}")
+	} catch {
+		return {};
+	}
+}
+
+export function getV3ConfigValue(key: keyof V3Config) {
+	return getV3Config()[key];
+}
+
+export function setV3Config(config: Partial<V3Config>) {
+	config = {
+		...getV3Config(),
+		...config
+	}
+
+	fs.writeFileSync(configPath,JSON.stringify(config));
+
+	return config as V3Config;
+}
+
+export function setV3ConfigKey<T extends keyof V3Config,V extends V3Config[T]>(key: T,value: V) {
+	return setV3Config({
+		[key]: value
+	});
+}
