7e3eee459377c3cbf4bdd29503442da5c968131e
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 16:46:38 +0330

    update wallet/actions.ts

--- src/app/(version3)/@special/wallet/actions.ts
+++ src/app/(version3)/@special/wallet/actions.ts
@@ -3,7 +3,24 @@
 
 
 import {useAccount} from "wagmi";
+import prisma from "@backend/modules/prisma/Prisma";
+import {getUserFromCookies} from "@/utils/serverComponents/user";
 
 export async function setUserWallet(acc:Omit<ReturnType<typeof useAccount>, 'connector'>) {
-
+	const user = await getUserFromCookies();
+	if (!user) return;
+	await prisma.user.update({
+		where: {
+			id: user.id
+		},
+		data: {
+			connectedWallets: {
+				connectOrCreate: {
+					where: {
+						un
+					}
+				}
+			}
+		}
+	})
 }
