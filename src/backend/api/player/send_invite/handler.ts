2aa56c233bb374029826de111abe253ee5d43d61
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 03:28:35 +0330

    update send_invite/handler.ts

--- src/backend/api/player/send_invite/handler.ts
+++ src/backend/api/player/send_invite/handler.ts
@@ -6,6 +6,7 @@ import {Markup} from "telegraf";
 import {User} from "@prisma/client";
 import {communityButton, getWebAppUrl} from "@/bot/classes/DodoClient";
 import {getUserFromCookies} from "@/utils/serverComponents/user";
+import {sendInvite} from "@backend/api/player/send_invite/actions";
 
 export default class UpgradeHandler extends Handler {
 	async handler() {
