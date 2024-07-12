70118387ab761d52cedce9c061434dacb9a0e59c
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 17:03:29 +0330

    update modal-windows/Boost.js

--- src/app/(version3)/components/modal-windows/Boost.js
+++ src/app/(version3)/components/modal-windows/Boost.js
@@ -53,7 +53,7 @@ function Boost() {
     };
 
     return (
-        <div id="boost" className="modal">
+        <div id="boost" className="modal" style={{opacity: 1}}>
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
