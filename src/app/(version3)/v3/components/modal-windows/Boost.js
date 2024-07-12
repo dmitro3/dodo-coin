338ec394bb3394787dd1af4b1afcba7b780b8e36
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 05:28:20 +0330

    update modal-windows/Boost.js

--- src/app/(version3)/v3/components/modal-windows/Boost.js
+++ src/app/(version3)/v3/components/modal-windows/Boost.js
@@ -1,6 +1,6 @@
 "use client";
 import React, {useState} from 'react';
-import {useRouter} from "next/link";
+import {useRouter} from "next/navigation";
 import {useAuth} from '../../hooks/useAuth';
 import {postData} from '../../utils/api';
 import Big from "big.js";
