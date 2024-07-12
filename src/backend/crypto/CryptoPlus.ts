6ba541e632be4d297d168280a73c926eebf9d6fb
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 08:40:52 +0330

    update crypto/CryptoPlus.ts

--- src/backend/crypto/CryptoPlus.ts
+++ src/backend/crypto/CryptoPlus.ts
@@ -2,6 +2,7 @@
 
 import {User} from "@prisma/client";
 import {getUserFromCookies} from "@/utils/serverComponents/user";
+import prisma from "@backend/modules/prisma/Prisma";
 
 
 const shopId = "PPJjJrocLrv3h7LM";
@@ -95,5 +96,21 @@ export async function createPosPayment(amount: number) {
 
 export async function checkPosPayment(id: string) {
 	const R = (await fetch(`https://api.cryptocloud.plus/v2/invoice/checkout/info?invoice_uuid=${id}`).then(r=>r.json())) as POSCreationResponse | undefined;
-	return R?.result?.invoice_status;
+	const st = R?.result?.invoice_status;
+	const pId = (R?.result.invoice_id || R?.result.uuid).split("-")?.at?.(-1);
+
+	if (st?.includes("paid")) {
+		const u = await getUserFromCookies();
+		await prisma.user.update({
+			where: {
+				id: u?.id || -1
+			},
+			data: {
+				tron_balance: {
+					increment: R?.result.amount
+				}
+			}
+		}).catch(console.error)
+	}
+	return st;
 }
