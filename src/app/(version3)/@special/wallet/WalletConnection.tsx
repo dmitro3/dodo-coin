1bb7ddfe44469456f81f0ed1125aff2d6503b11f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 16:53:58 +0330

    update wallet/WalletConnection.tsx

--- src/app/(version3)/@special/wallet/WalletConnection.tsx
+++ src/app/(version3)/@special/wallet/WalletConnection.tsx
@@ -16,7 +16,7 @@ const WalletConnection = (props:{
 			setVerified(true);
 			const finalAccount: Omit<typeof acc, 'connector'> = fromEntries(entries(acc).filter(([k,v])=>typeof v !== 'object'));
 			window.localStorage.setItem("lastAccount", JSON.stringify(finalAccount));
-			setUserWallet(finalAccount).catch(console.error);
+			// setUserWallet(finalAccount).catch(console.error);
 		}
 	}, [acc]);
 	useEffect(() => {
