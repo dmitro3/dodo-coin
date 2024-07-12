dac5530e1fd58a59eb0a2b9a63ed8ed10b233b66
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 06:38:36 +0330

    update crypto/CryptoPlus.ts

--- src/backend/crypto/CryptoPlus.ts
+++ src/backend/crypto/CryptoPlus.ts
@@ -1,11 +1,17 @@
 'use server';
 
+const shopId = "PPJjJrocLrv3h7LM";
+
 export async function createPosPayment(amount: number) {
 	return await fetch("https://api.cryptocloud.plus/v2/invoice/pos/create?locale=en", {
 		headers: {
 			"content-type": "application/json",
 		},
 		method: "POST",
-		
+		body: JSON.stringify({
+			"amount": amount,
+			"shop_id": shopId,
+			"currency": "USD"
+		})
 	})
 }
