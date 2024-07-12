992b6d1e4e5ab37e15ab9c67d986633c3e69ed56
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 00:09:25 +0330

    update wallet/WalletConnection.tsx

--- src/app/(version3)/@special/wallet/WalletConnection.tsx
+++ src/app/(version3)/@special/wallet/WalletConnection.tsx
@@ -51,7 +51,7 @@ const WalletConnection = (props: {
 							{account.address}
 						</div>
 						<button className={'border-red-400 text-red-400 rounded-lg'}>
-							<svg xmlns="http://www.w3.org/2000/svg" width={20} height={24} viewBox="0 0 24 24"
+							<svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24"
 								fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round"
 								strokeLinejoin="round"
 								className="icon icon-tabler icons-tabler-outline icon-tabler-x">
