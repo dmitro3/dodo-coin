d6d7a5ab5cb281f0f17de4163c71f12e3e31bf45
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-12 19:11:34 +0330

    update wallet/Verification.tsx

--- src/app/(version3)/@special/wallet/Verification.tsx
+++ src/app/(version3)/@special/wallet/Verification.tsx
@@ -78,7 +78,7 @@ export const WalletVerificationModal = () => {
 							</div>
 						))}
 						{state.error && (
-							<button className={'bg-black text-white'}>
+							<button className={'bg-black text-white border-white'}>
 								test
 							</button>
 						)}
