65a4ac43d4b52c39444f6e2193d4759f81d58594
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 17:39:45 +0330

    update (version3)/page.tsx

--- src/app/(version3)/page.tsx
+++ src/app/(version3)/page.tsx
@@ -9,9 +9,9 @@ const Page = () => {
 			</div>
 			<main className={'holder'}>
 				<div className="container" id="container" style={{position: "absolute"}}>
-					<div className="profileData p-4" style={{}}>
+					<div className="profileData " style={{}}>
 
-						<div className="profileData-name">
+						<div className="profileData-name p-4">
 							<div className="profileData-name-nick">itz_parsaw</div>
 							<div className="profileData-name-tag">@itz_parsaw</div>
 						</div>
