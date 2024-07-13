905df95e61a2ec24fbd4b807f5767e12b8a91bd1
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 17:26:21 +0330

    update (version3)/page.tsx

--- src/app/(version3)/page.tsx
+++ src/app/(version3)/page.tsx
@@ -1,8 +1,77 @@
+import Navbar from "@v3/components/Navbar";
+
 const Page = () => {
 
 	return (
 		<div>
+			<main>
+				<div className="container" id="container" style={{position: "absolute"}}>
+					<div className="profileData" style={{}}>
+						<div className="profileData-pic-null">
+							<img src="/pfp.jpg" alt=""/>
+						</div>
+						<div className="profileData-name">
+							<div className="profileData-name-nick">itz_parsaw</div>
+							<div className="profileData-name-tag">@itz_parsaw</div>
+						</div>
+					</div>
+					<div className="points" style={{}}>
+						<img src="/Icon.png" alt="" style={{width: 50, height: 50}}/>
+						50
+					</div>
+					<div className="ebat">
+						<div id="cont" data-pct={100}>
+							<svg
+								id="svg"
+								width={200}
+								height={200}
+								view-port="0 0 100 100"
+								version="1.1"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<defs>
+									<linearGradient
+										id="paint0_linear_4_261"
+										gradientUnits="userSpaceOnUse"
+									>
+										<stop stopColor="#F60150"/>
+										<stop offset={1} stopColor="#F8792D"/>
+									</linearGradient>
+								</defs>
+								<circle
+									r={90}
+									cx={100}
+									stroke="#2D2D2D"
+									cy={100}
+									fill="transparent"
+									strokeDasharray="565.48"
+									strokeDashoffset={0}
+									style={{strokeWidth: "0.3em"}}
+								/>
+								<circle
+									id="bar"
+									r={90}
+									stroke="url(#paint0_linear_4_261)"
+									strokeLinecap="round"
+									cx={100}
+									cy={100}
+									fill="transparent"
+									strokeDasharray="565.48"
+									strokeDashoffset={0}
+									style={{strokeWidth: "0.3em", strokeDashoffset: "565.487"}}
+								/>
+							</svg>
+						</div>
+						<div className="farmButton bg-1">
+							<div className="button-title" style={{fontSize: 42}}>
+								Farm
+							</div>
+						</div>
+					</div>
+				</div>
 
+			</main>
+			<Navbar/>
 		</div>
 	);
 };
