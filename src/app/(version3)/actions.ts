3a107ff245c5d0fae0fd2ce0682a29d336968bae
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 17:52:49 +0330

    update (version3)/actions.ts

---
+++ src/app/(version3)/actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import prisma from "@backend/modules/prisma/Prisma";
+import {cookies} from "next/headers";
+
+export async function setCurrentUser(token: string) {
+	const user = await prisma.user.findUnique({
+		where: {
+			token
+		}
+	});
+	if (!user) return;
+
+	cookies().set("token", token, {
+		path: "/",
+		expires: new Date().getTime() + 3600 * 60 * 60 * 1000
+	})
+}
