c9231b05e8f8c57bc62b96707ad19e0ea17cd6e2
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 03:24:12 +0330

    update status/ViewStatus.tsx

--- src/app/(admin)/admin/status/ViewStatus.tsx
+++ src/app/(admin)/admin/status/ViewStatus.tsx
@@ -2,52 +2,9 @@ import prisma from "@backend/modules/prisma/Prisma";
 import StatsGrid from "@/app/(admin)/admin/status/StatsGrid";
 
 const ViewStatus = async () => {
-
-	const today = await viewOfDate(new Date());
-
-	const d = new Date();
-	d.setDate(d.getDate() - 1);
-	const yesterday = await viewOfDate(d);
-
-	const _d1 = new Date();
-	_d1.setDate(_d1.getDate() - _d1.getDay());
-	const thisWeek = await viewOfDate(_d1,7);
-
-	const _d2 = new Date(_d1);
-	_d2.setDate(_d2.getDate() - 7);
-	const prevWeek = await viewOfDate(_d2,7);
-
-	const d1 = new Date();
-	d1.setDate(0);
-	const thisMonth = await viewOfDate(d1, 30);
-
-	const d2 = new Date(d1);
-	d2.setMonth(d2.getMonth() - 2);
-	const prevMonth = await viewOfDate(d2, 30);
-
 	return (
 		<div>
-			<h2 className={'text-bold text-2xl'}>Site View</h2>
-			<StatsGrid stats={[
-				{
-					value: today + "",
-					diff: today - yesterday,
-					title: "Today",
-					description: "Compared with yesterday views"
-				},
-				{
-					value: thisWeek + "",
-					diff: thisWeek - prevWeek,
-					title: "This Week",
-					description: "Compared with previous week views"
-				},
-				{
-					value: thisMonth + "",
-					diff: thisMonth - prevMonth,
-					title: "This Month",
-					description: "Compared with previous month views"
-				}
-			]}/>
+			<SiteView />
 			<h2 className={'text-bold text-2xl'}>Users</h2>
 			<StatsGrid stats={[
 				{
@@ -73,7 +30,7 @@ const ViewStatus = async () => {
 	);
 };
 
-async function viewOfDate(from: Date, toAfterDays = 1) {
+export async function recordOfDate(model: keyof typeof prisma,key: string,from: Date, toAfterDays = 1) {
 	from = new Date(from);
 	from.setHours(0);
 	from.setSeconds(0);
@@ -82,9 +39,10 @@ async function viewOfDate(from: Date, toAfterDays = 1) {
 	const to = new Date(from);
 	to.setDate(to.getDate() + toAfterDays);
 
-	return await prisma.siteView.count({
+	//@ts-ignore
+	return await prisma[model].count({
 		where: {
-			created_at: {
+			[key]: {
 				gte: from,
 				lte: to
 			}
