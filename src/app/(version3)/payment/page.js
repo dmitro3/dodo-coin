bb764a1a9d9751134fa648249d9319a6641c73d2
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 21:27:17 +0330

    update payment/page.js

--- src/app/(version3)/payment/page.js
+++ src/app/(version3)/payment/page.js
@@ -114,7 +114,7 @@ function PaymentPage(props) {
                                 <img src={semicircle} className={`show__icon spin${loading ? '' : ' hidden'}`} alt=""/>
                             </button>
                         </div>
-                        <div className="payment__sum"><span className="payment__value">{price.toString()}</span> USDT (ERC20)
+                        <div className="payment__sum"><span className="payment__value">{price.toString()}</span> USDT <span className={'text-xs'}>(ERC20)</span>
                         </div>
                         <span className="payment__tip">Send {price.toString()} USDT to this address:</span>
                         <a className="invite-link__copy blur"
