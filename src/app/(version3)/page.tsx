b3e59839a66702d61fb35bfaf9fae20687fe6572
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 18:49:18 +0330

    update (version3)/page.tsx

--- src/app/(version3)/page.tsx
+++ src/app/(version3)/page.tsx
@@ -9,7 +9,9 @@ const Page = async () => {
 
 	return (
 		<div className={'relative'}>
-
+			<div className="bg relative z-10">
+				<div className="slider-thumb"/>
+			</div>
 			<div className={'relative z-20'}>
 				<main className={'holder'}>
 					<div className="container flex flex-col gap-2 items-center" id="container">
@@ -37,9 +39,7 @@ const Page = async () => {
 				</main>
 				<Navbar/>
 			</div>
-			<div className="bg relative z-10">
-				<div className="slider-thumb"/>
-			</div>
+
 		</div>
 	);
 };
