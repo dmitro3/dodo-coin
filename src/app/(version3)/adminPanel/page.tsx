7b0840a1680d9c06ce73f4c8743fe229a4d7a79f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 02:41:48 +0330

    update adminPanel/page.tsx

--- src/app/(version3)/adminPanel/page.tsx
+++ src/app/(version3)/adminPanel/page.tsx
@@ -5,7 +5,7 @@ import {redirect} from "next/navigation";
 const Page = async () => {
 	const user = await getUserFromCookies();
 	if (user) redirect("/admin");
-	
+
 	return (
 		<div>
 			<SetUser />
