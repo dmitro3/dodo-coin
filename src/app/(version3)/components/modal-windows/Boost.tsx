95f13e5558093c621a3033441b4b2d36ea3f1e5b
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 20:57:51 +0330

    update modal-windows/Boost.tsx

--- src/app/(version3)/components/modal-windows/Boost.tsx
+++ src/app/(version3)/components/modal-windows/Boost.tsx
@@ -68,8 +68,10 @@ function Boost(props: {
 						<span className="modal-info">Here you can boost your per second profit</span>
 						<span
 							className="modal-info">current investment profitability is {user?.perSecondsProfit} per seconds and {((user?.perSecondsProfit || 0) * 60 * 60).toFixed(2)} per hour</span>
-
 						<br/>
+						<div>
+							<input className={'modal-input'} />
+						</div>
 						<div className={'flex gap-2 items-center'}>
 							<Link href="/payment"
 								 className={"modal-button modal-button__send " + (disabled ? ' disabled' : '')}
