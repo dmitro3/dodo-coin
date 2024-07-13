fb45f910d10c72155d10508d5d103ccae4f99dd3
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 03:41:02 +0330

    update friends/page.tsx

--- src/app/(version3)/friends/page.tsx
+++ src/app/(version3)/friends/page.tsx
@@ -166,9 +166,6 @@ const Page = async () => {
 														<div data-v-97259735="" className="kit-icon icon">
 															{f.joined_at.toLocaleDateString()}
 														</div>
-														<div data-v-97259735="" className="counter">
-															0
-														</div>
 													</div>
 												</div>
 												<div data-v-97259735="" className="balance">
