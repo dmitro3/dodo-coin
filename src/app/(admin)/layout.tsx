9c225f2974ce705753ee3a1f9190c7b4cbb23eeb
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 02:43:47 +0330

    update (admin)/layout.tsx

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,7 +1,9 @@
 import {getUserFromCookies} from "@/utils/serverComponents/user";
 import {DodoAdmins} from "@/bot/classes/DodoAdmin";
 import {redirect} from "next/navigation";
-
+import "../globals.css";
+import React from "react";
+import {MantineProvider} from "@mantine/core";
 const Layout = async (props: any) => {
 	const user = await getUserFromCookies();
 	if (!user || !DodoAdmins.includes(user.id)) redirect("/#deny")
