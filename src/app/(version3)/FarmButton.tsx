bf4fb8f36fbe9996414f67edc558bc093a1d8ce7
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 18:11:46 +0330

    update (version3)/FarmButton.tsx

--- src/app/(version3)/FarmButton.tsx
+++ src/app/(version3)/FarmButton.tsx
@@ -42,7 +42,7 @@ const FarmButton = () => {
 							fill="transparent"
 							strokeDasharray="565.48"
 							strokeDashoffset={0}
-							style={{strokeWidth: "1.05em", strokeDashoffset: "600"}}
+							style={{strokeWidth: "1.05em", strokeDashoffset: "350"}}
 						/>
 					</svg>
 
