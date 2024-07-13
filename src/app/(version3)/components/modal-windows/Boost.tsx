3edc9b69e773219adb67b784dfbecd18f5770dd3
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 21:14:16 +0330

    update modal-windows/Boost.tsx

--- src/app/(version3)/components/modal-windows/Boost.tsx
+++ src/app/(version3)/components/modal-windows/Boost.tsx
@@ -10,51 +10,22 @@ import {PrismaModelType} from "@backend/modules/prisma/Prisma";
 import {calcProfit} from "@/noside/profit";
 
 function Boost(props: {
-	user: PrismaModelType<'user'>
+	user: NonNullable<PrismaModelType<'user'>>
 }) {
 	let {user} = props;
 	const [price, setPrice] = useState(100);
 	const [want, setWant] = useState(10);
 	const [disabled, setDisabled] = useState(false);
-	const [displayText, setDisplayText] = useState('1.0 GH/s');
 	const navigate = useRouter();
 	const [sent, setSent] = useState(false);
-	const {userData} = useAuth();
 
-	const handleInputChange = (event: any) => {
-		const value = event.target.value.trim();
-
-		if (value === '') {
-			setWant(value);
-			setPrice(0);
-			setDisabled(true);
-			setDisplayText('1.0 GH/s');
-		} else {
-			const absValue = Math.abs(value);
-			setWant(absValue);
-			setPrice(absValue);
-			setDisabled(absValue < 100);
-
-			const power = Big(absValue).div(10);
-			// @ts-ignore
-			setDisplayText(power >= 1000 ? `${power.div(1000).toFixed(1)} TH/s` : `${power.toFixed(1)} GH/s`);
-		}
-	}
 
 	const handleAdd = async (e: any) => {
 		e.stopPropagation();
 		e.preventDefault();
-		let response;
-		try {
-			setSent(true);
-			response = await postData('/create_boost', {amount: price.toString()});
-		} catch (error) {
-			console.error('Error creating boost:', error);
-		} finally {
-			setSent(false);
 
-			navigate.push(`/payment?amount=${price}`);
-
+		if (user.usdtBalance >= want) {
+			
 		}
 	};
 
