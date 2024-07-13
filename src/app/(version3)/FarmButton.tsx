8ea3358768e1e4727889380a671e06389148d0c5
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 18:06:44 +0330

    update (version3)/FarmButton.tsx

--- src/app/(version3)/FarmButton.tsx
+++ src/app/(version3)/FarmButton.tsx
@@ -53,7 +53,7 @@ const FarmButton = () => {
 					<div className="button-title" style={{ fontSize: 42 }}>
 						Farming
 					</div>
-					<div className="collected">
+					<div className="collected mt-2">
 						<img src="/logo.webp" alt="" style={{ width: 25, height: 25 }} />
 						36.355
 					</div>
