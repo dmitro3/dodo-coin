eaed1e73b4e1eabb0261e553599c657ffc97dff7
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 21:32:36 +0330

    update payment/page.js

--- src/app/(version3)/payment/page.js
+++ src/app/(version3)/payment/page.js
@@ -24,7 +24,7 @@ function PaymentPage(props) {
             const amount = params.get('amount');
             createPosPayment(+amount).then(r => {
                 if (r.status === "success") {
-                    console.log(r.result.amount);
+                    console.log(r.result);
                     setAddress(r.result.address);
                     setPrice(r.result.amount);
                     setCreating(false);
