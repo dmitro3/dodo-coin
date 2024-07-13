a89e4bf3093e11dc848c3e94860b3f4b0bd3f0c0
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 17:53:25 +0330

    update (version3)/SetUser.tsx

--- src/app/(version3)/SetUser.tsx
+++ src/app/(version3)/SetUser.tsx
@@ -2,10 +2,12 @@
 
 import {useEffect} from "react";
 import {setCurrentUser} from "@v3/actions";
+import {useRouter} from "next/navigation";
 
 const SetUser = () => {
+	const router= useRouter();
 	useEffect(() => {
-		setCurrentUser(new URL(window.location.href).searchParams.get('token')+"").catch(console.error)
+		setCurrentUser(new URL(window.location.href).searchParams.get('token')+"").catch(console.error).then(router.refresh)
 	}, []);
 	return null;
 };
