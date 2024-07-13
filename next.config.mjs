4999a6fe0d03530450f809683b73f7fb3ab89a0a
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 01:23:52 +0330

    update dodo-coin/next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,7 @@ const nextConfig = {
                 set() {},
             });
         }
+        console.log(config.rules)
         config.module= {
             ...config.module,
             rules: [
