49d7b856286b5a847f12084d0daa12d7fa28d1da
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 20:02:35 +0330

    update (version3)/actions.ts

--- src/app/(version3)/actions.ts
+++ src/app/(version3)/actions.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@backend/modules/prisma/Prisma";
 import {cookies} from "next/headers";
+import {getUserFromCookies} from "@/utils/serverComponents/user";
 
 export async function setCurrentUser(token: string) {
 	const user = await prisma.user.findUnique({
@@ -16,4 +17,20 @@ export async function setCurrentUser(token: string) {
 	})
 
 	return true
+}
+
+
+export async function startFarm() {
+	const user = await getUserFromCookies();
+	if (!user) return;
+	if (user.isExpired) {
+		await prisma.user.update({
+			where: {
+				id: user.id,
+			},
+			data: {
+				farmStartAt: new Date()
+			}
+		})
+	}
 }
