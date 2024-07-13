64ceca112bfaedbf6b295a61b25dc0e7d08f1dba
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 00:26:55 +0330

    update (version3)/FarmButton.tsx

--- src/app/(version3)/FarmButton.tsx
+++ src/app/(version3)/FarmButton.tsx
@@ -180,7 +180,7 @@ const FarmButton = (props: {
 			) : (
 				<>
 					{user.farmed ? (
-						<div className={'flex items-center justify-center h-full w-full absolute'}>
+						<div className={'farmButton'}>
 							HALO
 						</div>
 					):(
