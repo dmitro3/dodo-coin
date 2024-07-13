dd1321ae584b789c1b2f144434f4a155d2b693fb
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 02:39:29 +0330

    update (admin)/layout.tsx

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,8 +1,10 @@
 import {getUserFromCookies} from "@/utils/serverComponents/user";
+import {DodoAdmins} from "@/bot/classes/DodoAdmin";
+import {redirect} from "next/navigation";
 
 const Layout = async (props: any) => {
 	const user = await getUserFromCookies();
-	if (user)
+	if (!user || !DodoAdmins.includes(user.id)) redirect("/#deny")
 	return props.children;
 };
 
