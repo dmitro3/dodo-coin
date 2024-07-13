02134987040a5f3926f8e825338b92af7c95520d
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 21:39:41 +0330

    update (version3)/ClaimButton.tsx

--- src/app/(version3)/ClaimButton.tsx
+++ src/app/(version3)/ClaimButton.tsx
@@ -1,15 +1,18 @@
 'use client'
 
 import {PrismaModelType} from "@backend/modules/prisma/Prisma";
+import {claimFarm} from "@v3/actions";
+import {useRouter} from "next/navigation";
 
 const ClaimButton = (props: {
 	user: PrismaModelType<'user'>
 }) => {
 	let {user} = props;
+	const router = useRouter();
 
 	return (
 		<button className={'flex-grow'} disabled={!user?.isExpired} onClick={()=>{
-			claimFarm();
+			claimFarm().then(router.refresh)
 		}}>
 			Claim
 		</button>
