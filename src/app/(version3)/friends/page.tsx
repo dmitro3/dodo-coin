9912657d094e4cc2912fe0eb4f0b962220bddf8f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 03:39:16 +0330

    update friends/page.tsx

--- src/app/(version3)/friends/page.tsx
+++ src/app/(version3)/friends/page.tsx
@@ -145,50 +145,53 @@ const Page = async () => {
 										1 fren
 									</div>
 									<div data-v-51de0b63="" className="list">
-										<div
-											data-v-97259735=""
-											data-v-51de0b63=""
-											className="pages-frens-list-item"
-										>
-											<div data-v-97259735="" className="avatar">
-												<div data-v-97259735="" className="letter">
-													I
-												</div>
-											</div>
-											<div data-v-97259735="" className="details">
-												<div data-v-97259735="" className="username">
-													itz_parsaw
-												</div>
-												<div data-v-97259735="" className="frens-count-compose">
-													<div data-v-97259735="" className="kit-icon icon">
-														<svg
-															width={24}
-															height={24}
-															viewBox="0 0 24 24"
-															fill="none"
-															xmlns="http://www.w3.org/2000/svg"
-														>
-															<g id="Style=Line">
-																<path
-																	id="Vector"
-																	d="M21 19.9999C21 18.2583 19.3304 16.7767 17 16.2275M15 20C15 17.7909 12.3137 16 9 16C5.68629 16 3 17.7909 3 20M15 13C17.2091 13 19 11.2091 19 9C19 6.79086 17.2091 5 15 5M9 13C6.79086 13 5 11.2091 5 9C5 6.79086 6.79086 5 9 5C11.2091 5 13 6.79086 13 9C13 11.2091 11.2091 13 9 13Z"
-																	stroke="white"
-																	strokeWidth={2}
-																	strokeLinecap="round"
-																	strokeLinejoin="round"
-																/>
-															</g>
-														</svg>
-													</div>
-													<div data-v-97259735="" className="counter">
-														0
+										{friends.map(f => (
+											<div
+												data-v-97259735=""
+												data-v-51de0b63=""
+												className="pages-frens-list-item"
+											>
+												<div data-v-97259735="" className="avatar">
+													<div data-v-97259735="" className="letter">
+														{f.username?.slice(0,1).toUpperCase()}
 													</div>
 												</div>
+												<div data-v-97259735="" className="details">
+													<div data-v-97259735="" className="username">
+														itz_parsaw
+													</div>
+													<div data-v-97259735=""
+														className="frens-count-compose">
+														<div data-v-97259735="" className="kit-icon icon">
+															<svg
+																width={24}
+																height={24}
+																viewBox="0 0 24 24"
+																fill="none"
+																xmlns="http://www.w3.org/2000/svg"
+															>
+																<g id="Style=Line">
+																	<path
+																		id="Vector"
+																		d="M21 19.9999C21 18.2583 19.3304 16.7767 17 16.2275M15 20C15 17.7909 12.3137 16 9 16C5.68629 16 3 17.7909 3 20M15 13C17.2091 13 19 11.2091 19 9C19 6.79086 17.2091 5 15 5M9 13C6.79086 13 5 11.2091 5 9C5 6.79086 6.79086 5 9 5C11.2091 5 13 6.79086 13 9C13 11.2091 11.2091 13 9 13Z"
+																		stroke="white"
+																		strokeWidth={2}
+																		strokeLinecap="round"
+																		strokeLinejoin="round"
+																	/>
+																</g>
+															</svg>
+														</div>
+														<div data-v-97259735="" className="counter">
+															0
+														</div>
+													</div>
+												</div>
+												<div data-v-97259735="" className="balance">
+													56 BP
+												</div>
 											</div>
-											<div data-v-97259735="" className="balance">
-												56 BP
-											</div>
-										</div>
+										))}
 									</div>
 								</div>
 
