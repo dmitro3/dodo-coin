6abba5006d0a684e28b5c2241271fad43aa57fb7
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-13 20:23:08 +0330

    update (version3)/page.tsx

--- src/app/(version3)/page.tsx
+++ src/app/(version3)/page.tsx
@@ -33,7 +33,7 @@ const Page = async () => {
 										<svg
 											id="Layer_1"
 											style={{
-												width: "10px",
+												width: "40px",
 											}}
 											data-name="Layer 1"
 											xmlns="http://www.w3.org/2000/svg"
