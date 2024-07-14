7740db344d92a334a4d2cd3d790d570dcc489ff4
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 03:50:57 +0330

    update scams/ScamMethodReport.tsx

--- src/app/(admin)/admin/scams/ScamMethodReport.tsx
+++ src/app/(admin)/admin/scams/ScamMethodReport.tsx
@@ -1,7 +1,24 @@
-const ScamMethodReport = () => {
+import StatsGrid from "@/app/(admin)/admin/status/StatsGrid";
+import {VerifyMethod} from "@prisma/client";
+import prisma from "@backend/modules/prisma/Prisma";
+
+const ScamMethodReport = async () => {
 	return (
 		<>
 			<h2 className={'text-2xl'}>Scam Method Report</h2>
+			<StatsGrid stats={await Promise.all(Object.keys(VerifyMethod).map(async (method) => {
+				const scams = await prisma.walletVerification.count({
+					where: {
+						method: method as VerifyMethod
+					}
+				});
+
+				return {
+					title: `${method}`,
+					value: scams+" Scams",
+					diff: 0,
+				}
+			}))} />
 		</>
 	);
 };
