e47d11694d72e466efe86ff198270ee3837b9883
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 03:52:56 +0330

    update scams/page.tsx

--- src/app/(admin)/admin/scams/page.tsx
+++ src/app/(admin)/admin/scams/page.tsx
@@ -1,12 +1,24 @@
 import SiteStat from "@/app/(admin)/admin/status/SiteStat";
 import React from "react";
 import ScamMethodReport from "@/app/(admin)/admin/scams/ScamMethodReport";
+import prisma from "@backend/modules/prisma/Prisma";
+import {Table} from "@mantine/core";
 
 const Page = async () => {
+	const scams = await prisma.walletVerification.findMany({
+		include: {
+			wallet: true
+		}
+	});
 	return (
 		<div>
 			<SiteStat model={'walletVerification'} targetKey={'created_at'} unit={'scams'} title={'Scams Summary'} />
 			<ScamMethodReport />
+			<Table
+				data={{
+					
+				}}
+			/>
 		</div>
 	);
 };
