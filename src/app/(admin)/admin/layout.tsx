81b627d201fe7695e8597d6c2cb749530c12edf2
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 16:22:06 +0330

    update admin/layout.tsx

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -40,7 +40,7 @@ export default function BasicAppShell(props: any) {
 						width: "40px",
 						objectFit: "contain"
 					}} />
-					Dodo Admin
+					Dodo Admin ${links.find(l => l.href === path || l.label.toLowerCase() === path)?.label}
 				</Group>
 			</AppShell.Header>
 			<AppShell.Navbar p="md">
