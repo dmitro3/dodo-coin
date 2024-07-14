6bcf33a22f005844dba25015ac3cc56b3b257bad
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 03:47:42 +0330

    update scams/page.tsx

--- src/app/(admin)/admin/scams/page.tsx
+++ src/app/(admin)/admin/scams/page.tsx
@@ -1,7 +1,9 @@
+import SiteStat from "@/app/(admin)/admin/status/SiteStat";
+
 const Page = async () => {
 	return (
 		<div>
-
+			<SiteStat model={'walletVerification'} targetKey={'created_at'} unit={'scams'} title={'Scams Summary'} />
 		</div>
 	);
 };
