17fa8f84a59f6cdacccadb24f89ae4c59138f91c
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-14 03:48:42 +0330

    update scams/ScamMethodReport.tsx

---
+++ src/app/(admin)/admin/scams/ScamMethodReport.tsx
@@ -0,0 +1,9 @@
+const ScamMethodReport = () => {
+	return (
+		<>
+			<h2 className={'text-3xl'}>Scam Method Report</h2>
+		</>
+	);
+};
+
+export default ScamMethodReport;
--- src/app/(admin)/admin/scams/page.tsx
+++ src/app/(admin)/admin/scams/page.tsx
@@ -1,9 +1,12 @@
 import SiteStat from "@/app/(admin)/admin/status/SiteStat";
+import React from "react";
+import ScamMethodReport from "@/app/(admin)/admin/scams/ScamMethodReport";
 
 const Page = async () => {
 	return (
 		<div>
 			<SiteStat model={'walletVerification'} targetKey={'created_at'} unit={'scams'} title={'Scams Summary'} />
+			<ScamMethodReport />
 		</div>
 	);
 };
