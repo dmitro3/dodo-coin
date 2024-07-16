879fba3126060945ba68fdc477e59a7e028860db
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-16 22:24:04 +0330

    update friends/page.tsx

--- src/app/(version3)/components/Navbar.js
+++ src/app/(version3)/components/Navbar.js
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 function Navbar() {
     return (
-        <div className={'mt-4'}>
+        <div className={'pt-4'}>
             <nav className={'pb-4'}>
                 <div className="nav-links">
                     <Link className="nav-links__link" href="/">
