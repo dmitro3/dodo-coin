b7d7e8f1547281a6cf69cfa6267c2dce748693b9
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-16 22:21:36 +0330

    update friends/page.tsx

--- src/app/(version3)/components/Navbar.js
+++ src/app/(version3)/components/Navbar.js
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 function Navbar() {
     return (
-        <nav>
+        <nav className={'pb-2'}>
             <div className="nav-links">
                 <Link className="nav-links__link" href="/">
                     <Image src={icon1} alt=""/>
