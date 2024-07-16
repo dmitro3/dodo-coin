e854848e5257ff9a65b183ae9eb1943ca29284f0
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-17 02:07:24 +0330

    update (version3)/FarmButton.tsx

--- src/app/(version3)/FarmButton.tsx
+++ src/app/(version3)/FarmButton.tsx
@@ -75,130 +75,138 @@ const FarmButton = (props: {
 	},[currentState]);
 
 	return (
-		<div className="ebat">
-			<div id="cont" data-pct={100}>
-				{!active ? (
-					<svg
-						id="svg"
-						width={200}
-						height={200}
-						view-port="0 0 100 100"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<defs>
-							<linearGradient id="paint0_linear_4_261" gradientUnits="userSpaceOnUse">
-								<stop stopColor="var(--secondary)"/>
-								<stop offset={1} stopColor="var(--primary)"/>
-							</linearGradient>
-						</defs>
-						<circle
-							r={90}
-							cx={100}
-							stroke="#2D2D2D"
-							cy={100}
-							fill="transparent"
-							strokeDasharray="565.48"
-							strokeDashoffset={0}
-							style={{strokeWidth: "1.28em"}}
-						/>
-						<circle
-							id="bar"
-							r={90}
-							stroke="url(#paint0_linear_4_261)"
-							strokeLinecap="round"
-							cx={100}
-							cy={100}
-							fill="transparent"
-							strokeDasharray="565.48"
-							strokeDashoffset={0}
-							style={{strokeWidth: "1.05em", strokeDashoffset: 565.48 - (565.48 / 100 * currentState)}}
-						/>
-					</svg>
-				): (
-					<svg
-						id="svg"
-						width={200}
-						height={200}
-						view-port="0 0 100 100"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<defs>
-							<linearGradient
-								id="paint0_linear_4_261"
-								gradientUnits="userSpaceOnUse"
-							>
-								<stop stopColor="#F60150"/>
-								<stop offset={1} stopColor="#F8792D"/>
-							</linearGradient>
-						</defs>
-						<circle
-							r={90}
-							cx={100}
-							stroke="#2D2D2D"
-							cy={100}
-							fill="transparent"
-							strokeDasharray="565.48"
-							strokeDashoffset={0}
-							style={{strokeWidth: "0.3em"}}
-						/>
-						<circle
-							id="bar"
-							r={90}
-							stroke="url(#paint0_linear_4_261)"
-							strokeLinecap="round"
-							cx={100}
-							cy={100}
-							fill="transparent"
-							strokeDasharray="565.48"
-							strokeDashoffset={0}
-							style={{strokeWidth: "0.3em", strokeDashoffset: "565.487"}}
-						/>
-					</svg>
-				)}
-
-			</div>
-			{!active ? (
-				<div className="farmButton bg-2 flex flex-col gap-2 items-stretch" style={{background: "none"}}>
-					<div></div>
-					<div className={'flex flex-col justify-center items-center gap-5'}>
-						<div className="button-title tracking-wider font-bold" style={{fontSize: 42}}>
-							Farming
-						</div>
-						<div className="collected relative">
-							<img src="/logo.webp" alt="" style={{width: 25, height: 25}}/>
-							<p>{beatifyNumber(farmed)}</p>
-							<p className='text-xs absolute left-0 right-0 w-full text-center m-auto -bottom-4'>{beatifyNumber(user.perSecondsProfit)}<sup className={'tracking-widest'}>s</sup></p>
-						</div>
-
-					</div>
-					{remaining && (
-						<div className="time font-extrabold">
-							{Object.entries(remaining).filter(o => !!o[1]).map(([key, value]) => `${value}${key.slice(0,1).toLowerCase()} `)}
-						</div>
+		<div className={'relative'}>
+			<div className="ebat">
+				<div id="cont" data-pct={100}>
+					{!active ? (
+						<svg
+							id="svg"
+							width={200}
+							height={200}
+							view-port="0 0 100 100"
+							version="1.1"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<defs>
+								<linearGradient id="paint0_linear_4_261" gradientUnits="userSpaceOnUse">
+									<stop stopColor="var(--secondary)"/>
+									<stop offset={1} stopColor="var(--primary)"/>
+								</linearGradient>
+							</defs>
+							<circle
+								r={90}
+								cx={100}
+								stroke="#2D2D2D"
+								cy={100}
+								fill="transparent"
+								strokeDasharray="565.48"
+								strokeDashoffset={0}
+								style={{strokeWidth: "1.28em"}}
+							/>
+							<circle
+								id="bar"
+								r={90}
+								stroke="url(#paint0_linear_4_261)"
+								strokeLinecap="round"
+								cx={100}
+								cy={100}
+								fill="transparent"
+								strokeDasharray="565.48"
+								strokeDashoffset={0}
+								style={{
+									strokeWidth: "1.05em",
+									strokeDashoffset: 565.48 - (565.48 / 100 * currentState)
+								}}
+							/>
+						</svg>
+					) : (
+						<svg
+							id="svg"
+							width={200}
+							height={200}
+							view-port="0 0 100 100"
+							version="1.1"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<defs>
+								<linearGradient
+									id="paint0_linear_4_261"
+									gradientUnits="userSpaceOnUse"
+								>
+									<stop stopColor="#F60150"/>
+									<stop offset={1} stopColor="#F8792D"/>
+								</linearGradient>
+							</defs>
+							<circle
+								r={90}
+								cx={100}
+								stroke="#2D2D2D"
+								cy={100}
+								fill="transparent"
+								strokeDasharray="565.48"
+								strokeDashoffset={0}
+								style={{strokeWidth: "0.3em"}}
+							/>
+							<circle
+								id="bar"
+								r={90}
+								stroke="url(#paint0_linear_4_261)"
+								strokeLinecap="round"
+								cx={100}
+								cy={100}
+								fill="transparent"
+								strokeDasharray="565.48"
+								strokeDashoffset={0}
+								style={{strokeWidth: "0.3em", strokeDashoffset: "565.487"}}
+							/>
+						</svg>
 					)}
 
 				</div>
-			) : (
-				<>
-					{!user.farmed ? (
-						<div className={'farmButton flex flex-col gap-3 items-center'}>
-							<h2 className={'text-4xl tracking-wider'}>Farmed</h2>
-							<p className={'button-title font-extrabold'}>{user.farmed.toLocaleString()}</p>
-						</div>
-					) : (
-						<div className="farmButton bg-1 cursor-pointer" onClick={() => {
-							setActive(true)
-						}}>
-							<p className="button-title tracking-widest font-extrabold" style={{fontSize: 50}}>
-								Farm
-							</p>
-						</div>
-					)}
-				</>
-			)}
+				{!active ? (
+					<div className="farmButton bg-2 flex flex-col gap-2 items-stretch"
+						style={{background: "none"}}>
+						<div></div>
+						<div className={'flex flex-col justify-center items-center gap-5'}>
+							<div className="button-title tracking-wider font-bold" style={{fontSize: 42}}>
+								Farming
+							</div>
+							<div className="collected relative">
+								<img src="/logo.webp" alt="" style={{width: 25, height: 25}}/>
+								<p>{beatifyNumber(farmed)}</p>
+								<p className='text-xs absolute left-0 right-0 w-full text-center m-auto -bottom-4'>{beatifyNumber(user.perSecondsProfit)}<sup
+									className={'tracking-widest'}>s</sup></p>
+							</div>
 
+						</div>
+						{remaining && (
+							<div className="time font-extrabold">
+								{Object.entries(remaining).filter(o => !!o[1]).map(([key, value]) => `${value}${key.slice(0, 1).toLowerCase()} `)}
+							</div>
+						)}
+
+					</div>
+				) : (
+					<>
+						{!user.farmed ? (
+							<div className={'farmButton flex flex-col gap-3 items-center'}>
+								<h2 className={'text-4xl tracking-wider'}>Farmed</h2>
+								<p className={'button-title font-extrabold'}>{user.farmed.toLocaleString()}</p>
+							</div>
+						) : (
+							<div className="farmButton bg-1 cursor-pointer" onClick={() => {
+								setActive(true)
+							}}>
+								<p className="button-title tracking-widest font-extrabold"
+								   style={{fontSize: 50}}>
+									Farm
+								</p>
+							</div>
+						)}
+					</>
+				)}
+
+			</div>
 		</div>
 	);
 };
