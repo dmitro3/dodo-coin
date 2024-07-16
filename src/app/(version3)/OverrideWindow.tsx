10e9a93ec74b19335ae5a324136392c788dd1274
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-17 01:15:32 +0330

    update (version3)/OverrideWindow.tsx

--- src/app/(version3)/OverrideWindow.tsx
+++ src/app/(version3)/OverrideWindow.tsx
@@ -1,8 +1,29 @@
-const OverrideWindow = () => {
-	return (
-		<div>
+import {useEffect, useState} from "react";
 
-		</div>
+const OverrideWindow = () => {
+	const [style, setStyle] = useState("");
+	useEffect(()=>{
+		const origin = window.open;
+
+		//@ts-ignore
+		window.open = (href,target,o)=>{
+			origin(href,'_blank',o);
+		}
+
+		if (window.Telegram) {
+			window.Telegram.WebApp.onEvent("viewportChanged", ()=>{
+				setStyle(`
+				:root {
+				--vh: ${window.Telegram.WebApp.viewportHeight}px
+				}
+				`);
+			})
+		}
+	}, [])
+	return (
+		<>
+			<style dangerouslySetInnerHTML={{__html: style}}></style>
+		</>
 	);
 };
 
