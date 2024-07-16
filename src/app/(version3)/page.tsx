ffe06ac87ca09fa88c7fd325b2cf49d3c1835cea
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-17 02:33:47 +0330

    update (version3)/page.tsx

--- src/app/(version3)/page.tsx
+++ src/app/(version3)/page.tsx
@@ -17,7 +17,7 @@ const Page = async () => {
 					<div className="bg relative z-0">
 						<div className="slider-thumb"/>
 					</div>
-					<div className=" flex flex-col gap-6 items-center justify-between relative z-20 min-h-screen overflow-hidden">
+					<div className=" flex flex-col gap-6 items-center justify-between relative z-20 min-h-[90vh] overflow-hidden">
 						<div className="profileData " style={{}}>
 
 							<div className="profileData-name p-2 px-4">
