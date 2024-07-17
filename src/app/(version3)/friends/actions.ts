5718ba86e979a134f31150ed5868496447ec55e9
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-18 02:57:30 +0330

    update friends/actions.ts

--- src/app/(version3)/friends/actions.ts
+++ src/app/(version3)/friends/actions.ts
@@ -17,6 +17,7 @@ export async function getFriends() {
 					refs: true
 				}
 			}
-		}
+		},
+		take: 10
 	})
 }
--- src/app/(version3)/friends/page.tsx
+++ src/app/(version3)/friends/page.tsx
@@ -11,6 +11,7 @@ import {PerFriendBonus} from "@/bot/classes/DodoBot";
 const Page = async () => {
 	const friends = await getFriends();
 
+
 	const empty = friends.length === 0;
 
 	return (
