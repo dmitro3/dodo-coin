3923319572fd61768a358111f7ea75bf424a4d98
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-18 10:55:39 +0330

    update open/page.tsx

--- src/app/(version3)/open/page.tsx
+++ src/app/(version3)/open/page.tsx
@@ -5,20 +5,21 @@ import {useEffect, useState} from "react";
 
 const Page = () => {
 	const url = new URL(window.location.href);
+	const [msg, setMsg] = useState("");
 	const [loading, setLoading] = useState(true)
 
 	useEffect(()=>{
 		try {
 			window.open(url.searchParams.get("url")+"","_self","noreferer");
-			setLoading(false);
+			setMsg("Close this tab, You can back to telegram.");
 		} catch {
-			
+			setMsg("Seems like you don't have this wallet!");
 		}
 	}, [url])
 
-	return loading ? <LoadingOverlay />:(
+	return !msg ? <LoadingOverlay />:(
 		<div className={'h-screen w-screen flex items-center justify-center'}>
-			Close this tab, You can back to telegram.
+			{msg}
 		</div>
 	);
 };
