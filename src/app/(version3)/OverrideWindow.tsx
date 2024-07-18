b78812fa96aeefe7fe714dae54d37c3e5c344f8b
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-18 11:20:34 +0330

    update (version3)/OverrideWindow.tsx

--- src/app/(version3)/OverrideWindow.tsx
+++ src/app/(version3)/OverrideWindow.tsx
@@ -56,7 +56,7 @@ const OverrideWindow = () => {
 		<>
 			<style dangerouslySetInnerHTML={{__html: style}}></style>
 			{!!exitLink && (
-				<div className={'fixed flex-col h-screen w-screen bg-black text-white flex items-center justify-center text-lg z-[999999]'}>
+				<div className={'fixed flex-col gap-2 p-3 h-screen w-screen bg-black text-white flex items-center justify-center text-lg z-[999999]'}>
 					<Image width={500} height={500} src={spinner} alt="" id="spinner"
 						  className="spinner__image"
 						  draggable="false"/>
