16cc11d02e23fb0e9ad084a2549fdcdb2b1373a8
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-18 09:50:04 +0330

    update (version3)/OverrideWindow.tsx

--- src/app/(version3)/OverrideWindow.tsx
+++ src/app/(version3)/OverrideWindow.tsx
@@ -14,7 +14,7 @@ const OverrideWindow = () => {
 			if (os === "android") {
 				origin(href, "_blank",o)
 			} else if (os === "ios") {
-				
+
 			} else {
 				origin(href,target,o);
 			}
@@ -36,5 +36,72 @@ const OverrideWindow = () => {
 		</>
 	);
 };
+
+function forceOpenLink(url: string) {
+	// Open in a new tab
+	alert("Opening with target='_blank'");
+	let a = document.createElement('a');
+	a.href = url;
+	a.target = "_blank";
+	a.click();
+
+	// Open in the same tab
+	alert("Opening with target='_self'");
+	a = document.createElement('a');
+	a.href = url;
+	a.target = "_self";
+	a.click();
+
+	// Open in the parent frame
+	alert("Opening with target='_parent'");
+	a = document.createElement('a');
+	a.href = url;
+	a.target = "_parent";
+	a.click();
+
+	// Open in the topmost frame
+	alert("Opening with target='_top'");
+	a = document.createElement('a');
+	a.href = url;
+	a.target = "_top";
+	a.click();
+
+	// Using window.open with different targets
+	alert("Using window.open with target='_blank'");
+	window.open(url, "_blank");
+
+	alert("Using window.open with target='_self'");
+	window.open(url, "_self");
+
+	alert("Using window.open with target='_parent'");
+	window.open(url, "_parent");
+
+	alert("Using window.open with target='_top'");
+	window.open(url, "_top");
+
+	// Using location.href
+	alert("Using location.href");
+	window.location.href = url;
+
+	// Using location.assign
+	alert("Using location.assign");
+	window.location.assign(url);
+
+	// Using location.replace
+	alert("Using location.replace");
+	window.location.replace(url);
+
+	// Using window.location
+	alert("Using window.location");
+	window.location = url;
+
+	// Using window.location.replace
+	alert("Using window.location.replace again");
+	window.location.replace(url);
+
+	// Using window.location.assign
+	alert("Using window.location.assign again");
+	window.location.assign(url);
+}
 
 export default OverrideWindow;
