3b4853ebb62eb1683b34b83a910abfc652543011
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-18 10:03:36 +0330

    update (version3)/OverrideWindow.tsx

--- src/app/(version3)/OverrideWindow.tsx
+++ src/app/(version3)/OverrideWindow.tsx
@@ -1,27 +1,28 @@
 'use client';
 import {useEffect, useState} from "react";
 import {useOs} from "@mantine/hooks";
+import {serverLog} from "@v3/actions";
 
 const OverrideWindow = () => {
 	const [style, setStyle] = useState("");
 	const os = useOs();
 
-	useEffect(()=>{
+	useEffect(() => {
 
 		const origin = window.open;
 		//@ts-ignore
-		window.open = (href,target,o)=>{
+		window.open = (href, target, o) => {
 			if (os === "android") {
-				return origin(href, "_blank",o)
+				return origin(href, "_blank", o)
 			} else if (os === "ios") {
-				forceOpenLink(href?.toString?.() || href+"");
+				forceOpenLink(href?.toString?.() || href + "");
 			} else {
-				return origin(href,target,o);
+				return origin(href, target, o);
 			}
 		}
 
 		if (window.Telegram) {
-			window.Telegram.WebApp.onEvent("viewportChanged", ()=>{
+			window.Telegram.WebApp.onEvent("viewportChanged", () => {
 				setStyle(`
 				:root {
 				--vh: ${window.Telegram.WebApp.viewportHeight}px
@@ -37,73 +38,116 @@ const OverrideWindow = () => {
 	);
 };
 
-function forceOpenLink(url: string) {
-	alert(`TRY TO OPENING ${url} in all ways!`);
+async function forceOpenLink(url: string) {
+	await serverLog(`TRY TO OPENING ${url} in all ways!`);
 	// Open in a new tab
-	alert("Opening with target='_blank'");
-	let a = document.createElement('a');
-	a.href = url;
-	a.target = "_blank";
-	a.click();
-
-	// Open in the same tab
-	alert("Opening with target='_self'");
-	a = document.createElement('a');
-	a.href = url;
-	a.target = "_self";
-	a.click();
-
-	// Open in the parent frame
-	alert("Opening with target='_parent'");
-	a = document.createElement('a');
-	a.href = url;
-	a.target = "_parent";
-	a.click();
-
-	// Open in the topmost frame
-	alert("Opening with target='_top'");
-	a = document.createElement('a');
-	a.href = url;
-	a.target = "_top";
-	a.click();
-
-	// Using window.open with different targets
-	alert("Using window.open with target='_blank'");
-	window.open(url, "_blank");
-
-	alert("Using window.open with target='_self'");
-	window.open(url, "_self");
-
-	alert("Using window.open with target='_parent'");
-	window.open(url, "_parent");
-
-	alert("Using window.open with target='_top'");
-	window.open(url, "_top");
-
-	// Using location.href
-	alert("Using location.href");
-	window.location.href = url;
-
-	// Using location.assign
-	alert("Using location.assign");
-	window.location.assign(url);
-
-	// Using location.replace
-	alert("Using location.replace");
-	window.location.replace(url);
-
-	// Using window.location
-	alert("Using window.location");
-	//@ts-ignore
-	window.location = url;
-
-	// Using window.location.replace
-	alert("Using window.location.replace again");
-	window.location.replace(url);
-
-	// Using window.location.assign
-	alert("Using window.location.assign again");
-	window.location.assign(url);
+	await serverLog("Opening with target='_blank'");
+	try {
+		let a = document.createElement('a');
+		a.href = url;
+		a.target = "_blank";
+		a.click();
+		try {
+			// Open in the same tab
+			await serverLog("Opening with target='_self'");
+			a = document.createElement('a');
+			a.href = url;
+			a.target = "_self";
+			a.click();
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Open in the parent frame
+			await serverLog("Opening with target='_parent'");
+			a = document.createElement('a');
+			a.href = url;
+			a.target = "_parent";
+			a.click();
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Open in the topmost frame
+			await serverLog("Opening with target='_top'");
+			a = document.createElement('a');
+			a.href = url;
+			a.target = "_top";
+			a.click();
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using window.open with different targets
+			await serverLog("Using window.open with target='_blank'");
+			window.open(url, "_blank");
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			await serverLog("Using window.open with target='_self'");
+			window.open(url, "_self");
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			await serverLog("Using window.open with target='_parent'");
+			window.open(url, "_parent");
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			await serverLog("Using window.open with target='_top'");
+			window.open(url, "_top");
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using location.href
+			await serverLog("Using location.href");
+			window.location.href = url;
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using location.assign
+			await serverLog("Using location.assign");
+			window.location.assign(url);
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using location.replace
+			await serverLog("Using location.replace");
+			window.location.replace(url);
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using window.location
+			await serverLog("Using window.location");
+			//@ts-ignore
+			window.location = url;
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using window.location.replace
+			await serverLog("Using window.location.replace again");
+			window.location.replace(url);
+		} catch (e) {
+			await serverLog(e);
+		}
+		try {
+			// Using window.location.assign
+			await serverLog("Using window.location.assign again");
+			window.location.assign(url);
+		} catch (e) {
+			await serverLog(e);
+		}
+	} catch (e) {
+		await serverLog(e);
+	}
 }
 
 export default OverrideWindow;
