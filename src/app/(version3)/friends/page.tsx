5830710a517a340b1793fce798dabad8f88fd70d
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-19 09:53:53 +0330

    update friends/page.tsx

--- src/app/(version3)/friends/page.tsx
+++ src/app/(version3)/friends/page.tsx
@@ -96,7 +96,7 @@ const Page = async () => {
 									data-v-55492676=""
 									className="pages-frens-list-heading list-heading"
 								>
-									<div data-v-fa52f77d="" className="heading">
+									<div data-v-fa52f77d="" className="heading mt-1">
 										<Image
 											data-v-97c098d1=""
 											src={family}
