676af137970a81dcee562272386beebea158a136
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-07-19 09:50:22 +0330

    update friends/page.tsx

--- src/app/(version3)/friends/page.tsx
+++ src/app/(version3)/friends/page.tsx
@@ -101,7 +101,7 @@ const Page = async () => {
 											data-v-97c098d1=""
 											src={family}
 											alt="Friends emoji"
-											className="emoji w-[50px] mt-3"
+											className="emoji w-[50px] relative top-10"
 										/>
 										<div data-v-fa52f77d="" className="title">
 											Invite Frens
