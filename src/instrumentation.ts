6bf5d1570bd19e99adaca13ffde765f3801beb41
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-09-17 17:28:42 +0330

    مخل

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -16,17 +16,20 @@ export async function register() {
 	const all = await prisma.siteSetting.findMany();
 	global.siteConfig = Object.fromEntries(all.map(o => ([o.key, o.value]))) as V3Config;
 
+	console.log("DEV USER", DEV_USER)
+
 	type keyType = 'error' | 'log' | 'warn';
 	const registerLog = (key: keyType) => {
 		const origin = console[key] as typeof console.error;
 		return (...args: any[]) => {
+			origin(...args);
 			try {
+				if (!ADMIN_BOT) throw("ADMIN BOT NOT INIT");
+
 				ADMIN_BOT.waitToReady().then(async (me) => adminLog(`[${key?.toUpperCase()}] ` + args.map(o => typeof o === 'object' ? JSON.stringify(o, null, 2) : o + "").join("\n"))).catch((e) => {
-					origin(...args);
 					origin(e);
 				});
 			} catch (e) {
-				origin(...args);
 				origin(e);
 			}
 		};
